32269bf963cdf0a24616ee6178d47154fea04432
Morten Høfft <mhoefft@gbif.org>
2016-01-26 18:35:27 +0100

    refactor to nunjucks templates and modular css

---
+++ app/controllers/home/ocurrence/ocurrence.js
@@ -0,0 +1,16 @@
+var express = require('express'),
+    router = express.Router(),
+    log = rootRequire('config/log');
+
+module.exports = function (app) {
+    app.use('/', router);
+};
+
+router.get('/ocurrence', function (req, res) {
+    //log.warn({randomAttribute: 'is test'}, 'My messagr grom the home controller');
+    //log.error({randomAttribute: 'ERROR'}, 'My ERROR MESSAGE GOES HERE');
+    res.render('pages/ocurrence/ocurrence', {
+        title: 'Ocurrences',
+        message: 'yada yada'
+    });
+});
