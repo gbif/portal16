6908b41aaad92b1f24ab381cb47b098ad36bc07d
Morten Høfft <mhoefft@gbif.org>
2016-01-28 17:12:37 +0100

    js polyfill introduced for ie9. js added to menu

--- app/views/shared/layout/main/main.js
+++ app/views/shared/layout/main/main.js
@@ -2,6 +2,54 @@ var menu = require('../partials/navigation/navigation.js');
 //$ = require('jquery');
 
 
+
+
+
+
+
+
+
+//https://cdnjs.cloudflare.com/ajax/libs/classlist/2014.01.31/classList.min.js
+/*! modernizr 3.3.1 (Custom Build) | MIT *
+ * http://modernizr.com/download/?-classlist-setclasses !*/
+//!function(n,e,s){function o(n,e){return typeof n===e}function a(){var n,e,s,a,i,f,r;for(var c in l)if(l.hasOwnProperty(c)){if(n=[],e=l[c],e.name&&(n.push(e.name.toLowerCase()),e.options&&e.options.aliases&&e.options.aliases.length))for(s=0;s<e.options.aliases.length;s++)n.push(e.options.aliases[s].toLowerCase());for(a=o(e.fn,"function")?e.fn():e.fn,i=0;i<n.length;i++)f=n[i],r=f.split("."),1===r.length?Modernizr[r[0]]=a:(!Modernizr[r[0]]||Modernizr[r[0]]instanceof Boolean||(Modernizr[r[0]]=new Boolean(Modernizr[r[0]])),Modernizr[r[0]][r[1]]=a),t.push((a?"":"no-")+r.join("-"))}}function i(n){var e=r.className,s=Modernizr._config.classPrefix||"";if(c&&(e=e.baseVal),Modernizr._config.enableJSClass){var o=new RegExp("(^|\\s)"+s+"no-js(\\s|$)");e=e.replace(o,"$1"+s+"js$2")}Modernizr._config.enableClasses&&(e+=" "+s+n.join(" "+s),c?r.className.baseVal=e:r.className=e)}var t=[],l=[],f={_version:"3.3.1",_config:{classPrefix:"",enableClasses:!0,enableJSClass:!0,usePrefixes:!0},_q:[],on:function(n,e){var s=this;setTimeout(function(){e(s[n])},0)},addTest:function(n,e,s){l.push({name:n,fn:e,options:s})},addAsyncTest:function(n){l.push({name:null,fn:n})}},Modernizr=function(){};Modernizr.prototype=f,Modernizr=new Modernizr;var r=e.documentElement;Modernizr.addTest("classlist","classList"in r);var c="svg"===r.nodeName.toLowerCase();a(),i(t),delete f.addTest,delete f.addAsyncTest;for(var u=0;u<Modernizr._q.length;u++)Modernizr._q[u]();n.Modernizr=Modernizr}(window,document);
+//
+//if (Modernizr.classlist) {
+//    alert(Modernizr.classlist);
+//} else {
+//    alert(Modernizr.classlist);
+//}
+(function() {
+    function appendScript(conditionalScript) {
+        var el = document.createElement('script');
+        el.setAttribute('src', conditionalScript);
+        document.head.appendChild(el);
+    }
+
+    //We wan't classlist and this is not supported in ie9
+    if (!document.body.classList) {
+        appendScript('//cdnjs.cloudflare.com/ajax/libs/classlist/2014.01.31/classList.min.js');
+    }
+})();
+
+
+
+//Matches there are vendor prefixes and no suport in ie9
+this.Element && function(ElementPrototype) {
+    ElementPrototype.matchesSelector = ElementPrototype.matchesSelector ||
+    ElementPrototype.mozMatchesSelector ||
+    ElementPrototype.msMatchesSelector ||
+    ElementPrototype.oMatchesSelector ||
+    ElementPrototype.webkitMatchesSelector ||
+    function (selector) {
+        var node = this, nodes = (node.parentNode || node.document).querySelectorAll(selector), i = -1;
+        while (nodes[++i] && nodes[i] != node);
+        return !!nodes[i];
+    }
+}(Element.prototype);
+
+
+
 /**
  This method should catch all, but the line number and column might not be supported in all browsers. Test this
 
@@ -51,137 +99,123 @@ module.exports = {
 
 //Create a global GBIF Object
 (function (global) {
-    var gb = {
-        VERSION: '0.0.1'
-    };
+    var gb = gb || {},
+        util = {VERSION: '0.0.1'};
 
-    gb.addEventListener = function (el, eventName, handler) {
-        if (el.addEventListener) {
+
+    //event listeners
+    util.addEventListenerAll = function (selector, eventName, handler) {
+        util.forEachElement(selector, function (el) {
             el.addEventListener(eventName, handler);
-        } else {
-            el.attachEvent('on' + eventName, function () {
-                handler.call(el);
-            });
-        }
-    };
-
-    gb.addEventListenerAll = function (selector, eventName, handler) {
-        gb.forEachElement(selector, function (el) {
-            gb.addEventListener(el, eventName, handler);
         })
     };
 
-    gb.forEachElement = function (selector, fn) {
+    //misc
+    util.forEachElement = function (selector, fn) {
         var elements = document.querySelectorAll(selector);
         for (var i = 0; i < elements.length; i++)
             fn(elements[i], i);
     };
 
-    gb.toggleClass = function (el, className) {
-        if (el.classList) {
-            el.classList.toggle(className);
-        } else {
-            var classes = el.className.split(' ');
-            var existingIndex = -1;
-            for (var i = classes.length; i--;) {
-                if (classes[i] === className)
-                    existingIndex = i;
-            }
-
-            if (existingIndex >= 0)
-                classes.splice(existingIndex, 1);
-            else
-                classes.push(className);
-
-            el.className = classes.join(' ');
-        }
-    };
-
-    gb.removeClass = function (el, className) {
-        if (el.classList)
-            el.classList.remove(className);
-        else
-            el.className = el.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+    //consider moving into polyfill
+    util.matches = function (el, selector) {
+        var p = Element.prototype;
+        var f = p.matches || p.webkitMatchesSelector || p.mozMatchesSelector || p.msMatchesSelector || function(s) {
+                return [].indexOf.call(document.querySelectorAll(s), this) !== -1;
+            };
+        return f.call(el, selector);
     };
 
 
-    //Expose the gbif helper object globally
-    if (global.gb) {
-        throw new Error('gb has already been defined');
-    } else {
-        global.gb = gb;
-    }
+    gb.util = util;
+    global.gb = gb;
 })(window);
 
 
 var searchToggleSelector = '.site__searchToggle',
     navToggleSelector = '.site__menuToggle';
 var toggleMenu = function () {
-    gb.toggleClass(document.getElementById('SiteHeader').querySelector('.site__menuToggle'), 'isActive');
-    gb.toggleClass(document.getElementById('siteCanvas'), 'hasActiveMenu');
+    document.getElementById('SiteHeader').classList.toggle('isActive');
+    document.getElementById('siteCanvas').classList.toggle('hasActiveMenu');
 
-    gb.forEachElement(searchToggleSelector, function (el) {
-        gb.removeClass(el, 'isActive');
+    gb.util.forEachElement(searchToggleSelector, function (el) {
+        el.classList.remove('isActive');
     });
 
     var searchAreaEl = document.getElementById('site_search');
-    gb.removeClass(searchAreaEl, 'isActive');
+    searchAreaEl.classList.remove('isActive');
 };
-gb.addEventListenerAll(navToggleSelector, 'click', toggleMenu);
-// gb.addEventListener(document.getElementById('content-overlay'), 'touchstart', toggleMenu);
-//toggleMenu();
+gb.util.addEventListenerAll(navToggleSelector, 'click', toggleMenu);
 
+
+function getAncestors(el, stopEl) {
+    var ancestors = [];
+    while ((el = el.parentElement) && el != stopEl) ancestors.push(el);
+    return ancestors;
+}
 
 //collapse and expand menu items
 var siteNav = document.getElementById('nav');
 var SiteNavCategoryItems = siteNav.querySelectorAll('.isCategory');
-gb.addEventListenerAll('.isCategory>a', 'click', function (event) {
-    var child, isParent, parent;
-    for (var i = 0; i < SiteNavCategoryItems.length; i++) {
+gb.util.addEventListenerAll('.isCategory>a', 'click', function (event) {
+    var ancestors = getAncestors(this, siteNav),
+        child, i;
+
+    //collpase all items that are not parents
+    for (i = 0; i < SiteNavCategoryItems.length; i++) {
         child = SiteNavCategoryItems[i];
-        isParent = false;
-        parent = this;
-
-        while (parent != siteNav && !isParent) {
-            parent = parent.parentNode;
-            isParent = parent == child;
-        }
-
-        if (!isParent) {
-            gb.removeClass(child, 'isExpanded');
+        if (ancestors.indexOf(child) == -1) {
+            child.classList.remove('isExpanded');
         }
     }
-    gb.toggleClass(this.parentNode, 'isExpanded');
+
+    if (!siteNav.classList.contains('isExpanded')) {
+        //for horizontal layout. When changing from laptop to mobile this means that the first menu click is ignored
+        this.parentNode.classList.add('isExpanded');
+    }
+    else {
+        this.parentNode.classList.toggle('isExpanded');
+    }
+    siteNav.classList.add('isExpanded');//use for horizontal layout
+    event.preventDefault(); //do not scroll to top
+});
+
+//collapse expand service menu
+gb.util.addEventListenerAll('.ServiceMenu__teaser>a', 'click', function (event) {
+    this.parentNode.parentNode.classList.toggle('isExpanded');
 });
 
 
-gb.addEventListenerAll(searchToggleSelector, 'click', function () {
-    gb.forEachElement(searchToggleSelector, function (el) {
-        gb.toggleClass(el, 'isActive');
+//Search toggling
+gb.util.addEventListenerAll(searchToggleSelector, 'click', function (event) {
+    gb.util.forEachElement(searchToggleSelector, function (el) {
+        el.classList.toggle('isActive');
     });
 
     var searchAreaEl = document.getElementById('site_search');
-    gb.toggleClass(searchAreaEl, 'isActive');
+    searchAreaEl.classList.toggle('isActive');
     searchAreaEl.querySelector('input').focus();
+    closeMenus();
+    event.preventDefault();
 });
 
-gb.addEventListener(document, 'click', function(event){
-    var clickInsideMenu = gb.selectorMatches(event.target, '.SiteNav .isCategory *');
-    if (!clickInsideMenu) {
-        for (var i = 0; i < SiteNavCategoryItems.length; i++) {
-            gb.removeClass(SiteNavCategoryItems[i], 'isExpanded');
-        }
+
+
+
+
+//close menu when clicking outside
+function closeMenus() {
+    siteNav.classList.remove('isExpanded');
+    if (document.getElementById('siteCanvas').classList.contains('hasActiveMenu')) {
+        toggleMenu();
     }
-});
-
-//Do we need the support for older browsers. this should suppor ie9 and forward
-function selectorMatches(el, selector) {
-    var p = Element.prototype;
-    var f = p.matches || p.webkitMatchesSelector || p.mozMatchesSelector || p.msMatchesSelector || function(s) {
-            return [].indexOf.call(document.querySelectorAll(s), this) !== -1;
-        };
-    return f.call(el, selector);
 }
+function closeMenusOnClickOutside(event) {
+    var clickOnContent = gb.util.matches(event.target, '#main *');
+    if (clickOnContent) {
+        closeMenus();
+    }
+}
+document.addEventListener('click', closeMenusOnClickOutside);
+document.addEventListener('touchend', closeMenusOnClickOutside);
 
-
-gb.selectorMatches = selectorMatches;
