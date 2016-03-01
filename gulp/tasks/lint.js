a3d4ecc252b890920e1a8f9206fef0cc1aa330f3
Morten Høfft <mhoefft@gbif.org>
2016-03-01 11:20:30 +0100

    eslint create checkstyle report for usage in jenkins

--- gulp/tasks/lint.js
+++ gulp/tasks/lint.js
@@ -1,19 +1,22 @@
 'use strict';
 
 var gulp = require('gulp'),
+    fs = require('fs'),
     config = rootRequire('config/build'),
     g = require('gulp-load-plugins')();
 
 gulp.task('server-lint', ['client-lint'], function() {
     return gulp.src(config.js.server.paths)
         .pipe(g.eslint())
-        .pipe(g.eslint.format())
+        .pipe(g.eslint.format()) // stdout
+        .pipe(g.eslint.format('checkstyle', fs.createWriteStream('reports/checkstyle_server.xml')))
         .pipe(g.size())
 });
 
 gulp.task('client-lint', function() {
     return gulp.src(config.js.client.paths)
         .pipe(g.eslint())
-        .pipe(g.eslint.format())
-        .pipe(g.size())
+        .pipe(g.eslint.format()) // stdout
+        .pipe(g.eslint.format('checkstyle', fs.createWriteStream('reports/checkstyle_client.xml')))
+        .pipe(g.size());
 });
