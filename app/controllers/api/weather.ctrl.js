3bbcf3e7f0401ba1633512a5abfd84cfb60e9872
Morten Hofft <mortenhoefft@gmail.com>
2016-04-19 09:00:20 +0200

    weather api from firecast.io - just a test

---
+++ app/controllers/api/weather.ctrl.js
@@ -0,0 +1,31 @@
+var express = require('express'),
+    router = express.Router(),
+    request = require('request');
+
+module.exports = function (app) {
+    app.use('/api', router);
+};
+
+router.get('/weather/:lat/:lng/:unix', function (req, res) {
+    var lat = req.params.lat,
+        lng = req.params.lng,
+        unix = req.params.unix;
+        res.send('Something went wrong from the weather API.');
+        // request('https://api.forecast.io/forecast/a5a201e1758bb93ad4b2ff166f1bc7bb/'+lat+','+lng+','+unix+'?units=si', function(error, response, body) {
+        //     console.log(body);
+        //     if (response.statusCode !== 200) {
+        //         res.send('Something went wrong from the weather API.');
+        //     }
+        //     else {
+        //         body = JSON.parse(body);
+        //         res.json(body);
+        //     }
+        // });
+});
+
+function renderPage(res, occurrence) {
+    res.json('pages/occurrence/key/occurrenceKey', {
+        occurrence: occurrence,
+        hasTools: true
+    });
+}
