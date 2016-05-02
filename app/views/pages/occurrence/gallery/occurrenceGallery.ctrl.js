e119620614377d8a7ff2bd65b3bdb04a8f7d97a7
Morten Hofft <mortenhoefft@gmail.com>
2016-05-02 08:34:25 +0200

    fit ratio with 5 types

--- app/views/pages/occurrence/gallery/occurrenceGallery.ctrl.js
+++ app/views/pages/occurrence/gallery/occurrenceGallery.ctrl.js
@@ -7,11 +7,23 @@ angular
     return {
         restrict: 'A',
         link: function(scope, element, attrs) {
+            var thinner = 0.6,
+                thin = 0.8,
+                wide = 1.4,
+                wider = 1.8,
+                widest= 2.0;
+
             element.on('load', function() {
                 element.parent().css({
-                    'background-image': 'url(' + attrs.src +')',
-                    'width': element[0].naturalWidth + 'px'
+                    'background-image': 'url(' + attrs.src +')'
                 });
+                var ratio = (element[0].naturalWidth)/element[0].naturalHeight;
+                // element.parent().attr('data-width', 'wider');
+                if (ratio > widest) element.parent().attr('data-width', 'widest');
+                else if (ratio > wider && ratio <= widest) element.parent().attr('data-width', 'wider');
+                else if (ratio > wide && ratio <= wider) element.parent().attr('data-width', 'wide');
+                else if (ratio > thinner && ratio <= thin) element.parent().attr('data-width', 'thin');
+                else if (ratio <= thinner) element.parent().attr('data-width', 'thinner');
                 element.parent().addClass('isValid');
             });
         }
