1a0e31c0539cedd0a7afb23f752c17a61faba05d
Morten Høfft <mhoefft@gbif.org>
2016-05-18 13:25:33 +0200

    occurrence style and images

--- app/controllers/ocurrence/key/occurrenceKey.ctrl.js
+++ app/controllers/ocurrence/key/occurrenceKey.ctrl.js
@@ -23,7 +23,7 @@ function renderPage(req, res, occurrence) {
      res.json(occurrence);
     } else {
         var angularInitData = occurrence.record;//occurrenceDetails.getAngularInitData(occurrence);
-        res.render('pages/occurrence/key/occurrenceKey2', {
+        res.render('pages/occurrence/key/occurrenceKey', {
             occurrence: occurrence,
             occurrenceFields: occurrenceDetails.occurrenceFields,
             angularInitData: angularInitData,
