6c000c12818f1a0b05d5566a23d3f1ffb451f72c
Burke Chih-Jen Ko <burkeker@gmail.com>
2016-06-12 18:43:37 +0200

    Working nav positioning.

--- app/views/pages/dataset/datasetDetailsScrollSpy.ctrl.js
+++ app/views/pages/dataset/datasetDetailsScrollSpy.ctrl.js
@@ -7,12 +7,59 @@ angular
     .controller('scrollSpyCtrl', scrollSpyCtrl);
 
 /** @ngInject */
-function scrollSpyCtrl($scope, $document) {
-    $scope.toTheTop = function() {
-        $document.scrollTopAnimated(0, 750).then(function() {
-            console && console.log('Scrolled to the top');
-        });
-    }
+function scrollSpyCtrl($document) {
+    var vm = this;
+
+    var EventUtil = {
+        addHandler: function(element, type, handler) {
+            if (element.addEventListener) {
+                element.addEventListener(type, handler, false);
+            } else if (element.attachEvent) {
+                element.attachEvent('on' + type, handler);
+            } else {
+                element['on' + type] = handler;
+            }
+        }
+    };
+
+    var metadataStrip = document.getElementById('metadata'),
+        metaNav = document.getElementById('metadata-nav'),
+        metaContent = document.getElementById('metadata-content'),
+        metaNavBottomSpacing = 200,
+        offsetHeightMetaNavOccupied = 80 + metaNav.offsetHeight + metaNavBottomSpacing,
+        offsetTopMetadataStrip = metadataStrip.offsetTop + 60,
+        offsetHeightMetadataStrip = metadataStrip.offsetHeight,
+        offsetTopMetadataStripHighest = offsetHeightMetadataStrip - offsetHeightMetaNavOccupied,
+        rectMetadataStrip,
+        rectMetaNav;
+
+    EventUtil.addHandler(window, 'scroll', function($event){
+        if (document.compatMode == 'CSS1Compat') {
+            rectMetadataStrip = metadataStrip.getBoundingClientRect();
+            rectMetaNav = metaNav.getBoundingClientRect();
+
+            if (rectMetadataStrip.top < 80 && rectMetadataStrip.top > -offsetTopMetadataStripHighest) {
+                metaNav.style.position = 'fixed';
+                metaNav.style.top = '80px';
+            } else if (rectMetadataStrip.top < -offsetTopMetadataStripHighest) {
+                metaNav.style.position = 'absolute';
+                metaNav.style.top = '';
+                metaNav.style.bottom = metaNavBottomSpacing + 'px';
+            } else {
+                metaNav.style.position = '';
+                metaNav.style.top = '';
+                metaNav.style.bottom = '';
+            }
+
+
+            if (rectMetadataStrip.top < 80) {
+                metaContent.classList.add('col-md-offset-4');
+            } else {
+                metaContent.classList.remove('col-md-offset-4');
+            }
+            console.log(rectMetadataStrip.top + ', ' + rectMetadataStrip.bottom + ', ' + offsetHeightMetaNavOccupied + ', ' + offsetTopMetadataStrip + ', ' + offsetTopMetadataStripHighest);
+        }
+    });
 }
 
 module.exports = scrollSpyCtrl;
