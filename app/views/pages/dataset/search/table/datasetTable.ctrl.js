ea9e630bdfa663c3b76d4a9a799b8f12990fbc9d
Morten Høfft <mhoefft@gbif.org>
2016-06-24 17:10:20 +0200

    show featured datasets when no query performed

--- app/views/pages/dataset/search/table/datasetTable.ctrl.js
+++ app/views/pages/dataset/search/table/datasetTable.ctrl.js
@@ -13,12 +13,15 @@ angular
     .controller('datasetTableCtrl', datasetTableCtrl);
 
 /** @ngInject */
-function datasetTableCtrl(results, $stateParams, $state) {
+function datasetTableCtrl(results, $stateParams, $state, $http) {
     var vm = this,
         offset = parseInt($stateParams.offset) || 0;
 
     vm.count = results.count;
     vm.results = results.results;
+    vm.featuredDataSets = {
+        nodes: []
+    };
 
     //pagination
     vm.maxSize = 5;
@@ -30,6 +33,26 @@ function datasetTableCtrl(results, $stateParams, $state) {
         $stateParams.offset =  (vm.currentPage-1) * vm.limit;
         $state.go($state.current, $stateParams, {reload: true});
     };
+
+    vm.getFeatured = function() {
+        //http://www.gbif.org/featured-datasets/json
+        $http({
+            method: 'GET',
+            url: 'http://www.gbif.org/featured-datasets/json'
+        }).then(function successCallback(response) {
+            vm.featuredDataSets.nodes = response.data.nodes;
+        }, function errorCallback(response) {
+            //ignore any errors and just do not show feeatured datasets
+        });
+    };
+    var keys = Object.keys($stateParams).reduce(function(prev, curr){
+        var v = !!$stateParams[curr]? 1 : 0;
+        return prev + v;
+    }, 0);
+
+    if (keys == 1) {//lang key
+        vm.getFeatured();
+    }
 }
 
 module.exports = datasetTableCtrl;
