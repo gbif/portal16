2dff887233575a723fb5956f554d83ed0293a696
timrobertson100 <timrobertson100@gmail.com>
2016-07-07 16:49:53 +0200

    Update occurrenceMap.ctrl.js
    Changed to use EPSG:4326 projection for the map layers

--- app/views/pages/occurrence/map/occurrenceMap.ctrl.js
+++ app/views/pages/occurrence/map/occurrenceMap.ctrl.js
@@ -22,7 +22,7 @@ function occurrenceMapCtrl(leafletData, mapConstants, $httpParamSerializer, Occu
     var getOverlay = function(query) {
         var overlay = {
             name: 'gb',
-            url: "http://api.gbif-uat.org/v1/map/occurrence/occurrence.png?x={x}&y={y}&z={z}&resolution=1&" + $httpParamSerializer(query),
+            url: "http://api.gbif-uat.org/v1/map/occurrence/tile.png?x={x}&y={y}&z={z}&srs=EPSG:4326&" + $httpParamSerializer(query),
             type: 'xyz',
             visible: true,
             layerParams: {
