95cff40b70c1e2f4a8f0a4608b262a0f6c0d7747
Burke Chih-Jen Ko <burkeker@gmail.com>
2016-08-03 11:11:25 +0200

    Error handling for URL lookup.

--- app/urlHandling/urlHandling.js
+++ app/urlHandling/urlHandling.js
@@ -10,59 +10,65 @@ module.exports = function (app) {
 router.get('/:requestedPath(*)', function(req, res, next) {
     // Start by looking up URL Alias
     request(cmsApi.urlLookup.url + req.params.requestedPath, function(e, r, b) {
-        b = JSON.parse(b);
-
-        // Only proceed to rendering if there is a valid result returned from URL lookup.
-        if (r.statusCode !== 200) {
-            res.send('URL lookup failed.');
-        }
-        else if (Object.prototype.toString.call(b.data[0]) === '[object Object]' && b.data.length == 1) {
-            var type = '';
-
-            switch (b.data[0].type) {
-                case 'news':
-                    type = 'news';
-                    break;
-                case 'data_use':
-                    type = 'data-use';
-                    break;
-                case 'event':
-                    type = 'event';
-                    break;
-                case 'resource':
-                    type = 'resource';
-                    break;
-                case 'gbif_participant':
-                    type = 'participant';
-                    break;
-                case 'page':
-                    type = 'page';
-                    break;
-                case 'programme':
-                    type = 'programme';
-                    break;
-                case 'project':
-                    type = 'project';
-                    break;
-                case 'external':
-                    type = 'external';
-                    break;
+        // @todo better error handling when CMS API endpoint is not available.
+        try {
+            b = JSON.parse(b);
+            // Only proceed to rendering if there is a valid result returned from URL lookup.
+            if (r.statusCode !== 200) {
+                res.send('URL lookup failed.');
             }
-            
-            // Only proceed to rendering if the requested path is identical to the target URL.
-            // Otherwise send a 301 redirection.
-            var redirectedUrl;
-            if (type == 'external') {
-                redirectedUrl = b.data[0].targetUrl;
+            else if (Object.prototype.toString.call(b.data[0]) === '[object Object]' && b.data.length == 1) {
+                var type = '';
+
+                switch (b.data[0].type) {
+                    case 'news':
+                        type = 'news';
+                        break;
+                    case 'data_use':
+                        type = 'data-use';
+                        break;
+                    case 'event':
+                        type = 'event';
+                        break;
+                    case 'resource':
+                        type = 'resource';
+                        break;
+                    case 'gbif_participant':
+                        type = 'participant';
+                        break;
+                    case 'page':
+                        type = 'page';
+                        break;
+                    case 'programme':
+                        type = 'programme';
+                        break;
+                    case 'project':
+                        type = 'project';
+                        break;
+                    case 'external':
+                        type = 'external';
+                        break;
+                }
+
+                // Only proceed to rendering if the requested path is identical to the target URL.
+                // Otherwise send a 301 redirection.
+                var redirectedUrl;
+                if (type == 'external') {
+                    redirectedUrl = b.data[0].targetUrl;
+                }
+                else {
+                    redirectedUrl = '/' + type + '/' + b.data[0].targetUrl;
+                }
+                res.redirect(301, redirectedUrl);
             }
             else {
-                redirectedUrl = '/' + type + '/' + b.data[0].targetUrl;
+                next();
             }
-            res.redirect(301, redirectedUrl);
+        } catch (e) {
+            next(e);
         }
-        else {
-            next();
-        }
+
+
 
     });
 
