d51997f721131e05703f0e9e24ff197593b9aad2
Burke Chih-Jen Ko <burkeker@gmail.com>
2016-09-21 13:26:26 +0200

    Merged CMS controllers into cmsContents.ctrl.js. Extra URL handling for old URLs starts with content type prefixes.

---
+++ app/controllers/cms/cmsContents.ctrl.js
@@ -0,0 +1,122 @@
+'use strict';
+var express = require('express'),
+    router = express.Router(),
+    format = require('../../helpers/format'),
+    cmsApi = require('../../models/cmsData/apiConfig'),
+    cmsData = require('../../models/cmsData/cmsData');
+
+module.exports = function (app) {
+    app.use('/', router);
+};
+
+router.get([
+        '/news/:requestedPath(*)',
+        '/data-use/:requestedPath(*)',
+        '/event/:requestedPath(*)',
+        '/page/:requestedPath(*)',
+        '/programme/:requestedPath(*)',
+        '/project/:requestedPath(*)',
+        '/participant/:requestedPath(*)',
+        '/resource/:requestedPath(*)'
+    ],
+    function(req, res, next) {
+    var originalUrl,
+        requestedPath,
+        jsonOutput = false;
+
+    // If there is '.debug' then later output data as JSON
+    if (req.params.requestedPath.search(/\.debug/) != -1) {
+        requestedPath = req.params.requestedPath.replace('.debug', '');
+        originalUrl = req.originalUrl.replace('.debug', '');
+        jsonOutput = true;
+    }
+    else {
+        // requestedPath is the captured alias in this route
+        requestedPath = req.params.requestedPath;
+        // originalURL is used as the second attempt for checking existing alias
+        originalUrl = req.originalUrl;
+    }
+
+    if (originalUrl.substring(0, 1) == '/') {
+        var originalUrlForLookup = originalUrl.slice(1);
+    }
+
+    // Many old URLs start with the content type prefix so we need to handle the redirection here if
+    // it doesn't captured by the generic urlHandling(/app/urlHandling).
+    // For an old URL, it's usually captured with originalUrl and redirected to an URL with
+    // prefix of the content type in the new site. So rendering happens when the targetUrl matches
+    // requestedUrl.
+    cmsData.cmsEndpointAccess(cmsApi.urlLookup.url + requestedPath)
+        .then(function(data){
+            if (typeof data.data[0] == 'object' && typeof data.data[0].targetUrl == 'string' ) {
+                if (data.data[0].targetUrl == requestedPath) {
+                    var contentType = data.data[0].type;
+                    var nid = data.data[0].id;
+                    contentType = (contentType == 'data_use') ? 'dataUse' : contentType;
+                    contentType = (contentType == 'gbif_participant') ? 'participant' : contentType;
+                    return cmsData.cmsEndpointAccess(cmsApi[contentType].url + nid);
+                }
+                else {
+                    return cmsData.cmsEndpointAccess(cmsApi.urlLookup.url + originalUrlForLookup);
+                }
+            }
+            else {
+                return cmsData.cmsEndpointAccess(cmsApi.urlLookup.url + originalUrlForLookup);
+            }
+        })
+        .then(function(data) {
+            // Render content if the result is not from URL Lookup.
+            if (typeof data.data[0] == 'object' && data.data[0].hasOwnProperty('created') && data.data[0].hasOwnProperty('title')) {
+                renderPage(data, jsonOutput);
+            }
+            // Redirect if the result is from URL Lookup.
+            else if (typeof data.data[0] == 'object' && typeof data.data[0].targetUrl == 'string') {
+                if (data.data[0].targetUrl == originalUrlForLookup) {
+                    // type-URL conversion
+                    var contentTypeUrlPrefix = (data.data[0].type == 'gbif_participant') ? data.data[0].type.replace('gbif_', '') : data.data[0].type.replace('_', '-');
+                    var redirectedUrl;
+                    if (contentTypeUrlPrefix == 'external') {
+                        redirectedUrl = data.data[0].targetUrl;
+                    }
+                    else {
+                        redirectedUrl = '/' + contentTypeUrlPrefix + '/' + data.data[0].targetUrl;
+                    }
+                    res.redirect(301, redirectedUrl);
+                }
+            }
+            else {
+                throw new Error('URL Alias lookup exception with originalUrlForLookup.');
+            }
+        })
+        .catch(function(err){
+            next(err);
+        });
+
+    function renderPage(body, jsonOutput) {
+        // Preprocess file size string (for resource)
+        if (body.data[0].file[0]) {
+            body.data[0].file[0].filesize = format.formatBytes(body.data[0].file[0].filesize, 1);
+        }
+        var proseContent = {
+            data: body.data[0],
+            images: body.data[0].images,
+            self: body.self,
+            _meta: {
+                title: body.data[0].title,
+                hasTools: true
+            }
+        };
+
+        try {
+            if (jsonOutput == true) {
+                res.json(proseContent);
+            } else {
+                var contentType = body.data[0].type;
+                contentType = (contentType == 'gbif_participant') ? 'participant' : contentType;
+                res.render('pages/about/' + contentType + '/' + contentType + '.nunjucks', proseContent);
+            }
+        } catch(e) {
+            next(e);
+        }
+    }
+});
--- app/urlHandling/urlHandling.js
+++ app/urlHandling/urlHandling.js
@@ -1,7 +1,7 @@
 var express = require('express'),
     router = express.Router(),
     cmsApi = require('../models/cmsData/apiConfig'),
-    request = require('request');
+    cmsData = require('../models/cmsData/cmsData');
 
 module.exports = function (app) {
     app.use('/', router);
@@ -9,67 +9,28 @@ module.exports = function (app) {
 
 router.get('/:requestedPath(*)', function(req, res, next) {
     // Start by looking up URL Alias
-    request(cmsApi.urlLookup.url + req.params.requestedPath, function(e, r, b) {
-        // @todo better error handling when CMS API endpoint is not available.
-        try {
-            b = JSON.parse(b);
-            // Only proceed to rendering if there is a valid result returned from URL lookup.
-            if (r.statusCode !== 200) {
-                res.send('URL lookup failed.');
-            }
-            else if (Object.prototype.toString.call(b.data[0]) === '[object Object]' && b.data.length == 1) {
-                var type = '';
+    cmsData.cmsEndpointAccess(cmsApi.urlLookup.url + req.params.requestedPath)
+        .then(function(body){
+            if (typeof body.data[0] == 'object' && typeof body.data[0].targetUrl == 'string') {
 
-                switch (b.data[0].type) {
-                    case 'news':
-                        type = 'news';
-                        break;
-                    case 'data_use':
-                        type = 'data-use';
-                        break;
-                    case 'event':
-                        type = 'event';
-                        break;
-                    case 'resource':
-                        type = 'resource';
-                        break;
-                    case 'gbif_participant':
-                        type = 'participant';
-                        break;
-                    case 'page':
-                        type = 'page';
-                        break;
-                    case 'programme':
-                        type = 'programme';
-                        break;
-                    case 'project':
-                        type = 'project';
-                        break;
-                    case 'external':
-                        type = 'external';
-                        break;
-                }
+                var contentTypeUrlPrefix = (body.data[0].type == 'gbif_participant') ? body.data[0].type.replace('gbif_', '') : body.data[0].type.replace('_', '-');
 
                 // Only proceed to rendering if the requested path is identical to the target URL.
                 // Otherwise send a 301 redirection.
                 var redirectedUrl;
-                if (type == 'external') {
-                    redirectedUrl = b.data[0].targetUrl;
+                if (body.data[0].type == 'external') {
+                    redirectedUrl = body.data[0].targetUrl;
                 }
                 else {
-                    redirectedUrl = '/' + type + '/' + b.data[0].targetUrl;
+                    redirectedUrl = '/' + contentTypeUrlPrefix + '/' + body.data[0].targetUrl;
                 }
                 res.redirect(301, redirectedUrl);
             }
             else {
-                next();
+                throw new Error('URL Alias lookup exception with requestedPath.');
             }
-        } catch (e) {
-            next(e);
-        }
-
-
-
-    });
-
+        })
+        .catch(function(err){
+            next(err);
+        });
 });
