473c42587e70e888e389b115384106c1e2c372ec
Morten Hofft <mortenhoefft@gmail.com>
2016-10-02 17:20:28 +0200

    js bug in dataset submenu - leading all js to fail sitewide on all other pages

--- app/views/pages/dataset/key/text/submenu.js
+++ app/views/pages/dataset/key/text/submenu.js
@@ -1,27 +1,30 @@
 module.exports = (function() {
     if (document.compatMode == 'CSS1Compat') {
         var navbarHeight = 60; //TODO should build stylus variables to js 
+        var submenuWrapper = document.getElementById('submenuWrapper');
         var submenu = document.getElementById('submenu');
-        var submenuHeight = submenu.scrollHeight;
-        window.addEventListener('scroll', positionSubmenu);
-        window.addEventListener('resize', positionSubmenu);
 
-        function positionSubmenu() {
-            var submenuWrapper = document.getElementById('submenuWrapper');
-            var submenu = document.getElementById('submenu');
-            var subMenuWrapperPosition = submenuWrapper.getBoundingClientRect();
-            var submenuPosition = submenu.getBoundingClientRect();
+        if (submenuWrapper && submenu) {
+            var submenuHeight = submenu.scrollHeight;
+            window.addEventListener('scroll', positionSubmenu);
+            window.addEventListener('resize', positionSubmenu);
 
-            if (subMenuWrapperPosition.bottom - submenuHeight < navbarHeight) {
-                submenu.classList.add('submenu--bottom');
-                submenu.classList.remove('submenu--fixed');
-            } else if (subMenuWrapperPosition.top > navbarHeight) {
-                submenu.classList.remove('submenu--fixed');
-                submenu.classList.remove('submenu--bottom');
-            } else if (subMenuWrapperPosition.top < navbarHeight) {
-                submenu.classList.add('submenu--fixed');
+            function positionSubmenu() {
+                var submenuWrapper = document.getElementById('submenuWrapper');
+                var submenu = document.getElementById('submenu');
+                var subMenuWrapperPosition = submenuWrapper.getBoundingClientRect();
+                var submenuPosition = submenu.getBoundingClientRect();
+
+                if (subMenuWrapperPosition.bottom - submenuHeight < navbarHeight) {
+                    submenu.classList.add('submenu--bottom');
+                    submenu.classList.remove('submenu--fixed');
+                } else if (subMenuWrapperPosition.top > navbarHeight) {
+                    submenu.classList.remove('submenu--fixed');
+                    submenu.classList.remove('submenu--bottom');
+                } else if (subMenuWrapperPosition.top < navbarHeight) {
+                    submenu.classList.add('submenu--fixed');
+                }
             }
-            console.log('round finished');
         }
     }
 })();
