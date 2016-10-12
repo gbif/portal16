b05cb28d9c169d005c8be5bf3a4d835994ce3c25
Morten Høfft <mhoefft@gbif.org>
2016-10-12 12:04:09 +0200

    markdown translations helper introduced

--- app/controllers/tools/suggestDataset/suggestDataset.ctrl.js
+++ app/controllers/tools/suggestDataset/suggestDataset.ctrl.js
@@ -1,15 +1,36 @@
+"use strict";
+
 var express = require('express'),
+    translationsHelper = rootRequire('app/helpers/translations'),
     router = express.Router();
 
 module.exports = function (app) {
     app.use('/', router);
 };
 
-router.get('/tools/suggest-dataset', function (req, res) {
-    res.render('pages/tools/suggestDataset/suggestDataset', {
-        _meta: {
-            title: 'Suggest dataset'
+router.get('/tools/suggest-dataset', function (req, res, next) {
+    //get text content
+    let markdownFiles = {
+        description: 'tools/suggestDataset/description/'
+    };
+    translationsHelper.getTranslations(markdownFiles, res.locals.gb.locales.current, function (err, translations) {
+        if (err) {
+            next(err);
+        } else {
+            render(req, res, next, {
+                _meta: {
+                    title: 'Suggest dataset'
+                },
+                translations: translations
+            });
         }
     });
 });
 
+function render(req, res, next, data) {
+    try {
+        res.render('pages/tools/suggestDataset/suggestDataset', data);
+    } catch (err) {
+        next(err);
+    }
+}
---
+++ app/helpers/translations.js
@@ -0,0 +1,45 @@
+"use strict";
+
+var marked = require('marked'),
+    async = require('async'),
+    fs = require('fs');
+
+function parseMarkdown(data, cb) {
+    try {
+        let renderedText = marked(data);
+        cb(null, renderedText);
+    } catch (err) {
+        cb(err)
+    }
+}
+
+function getTranslatedMarkdown(path, language, cb) {
+    fs.readFile(__dirname + '/../../locales/markdown/' + path + language + '.md', 'utf8', function (err, data) {
+        if (err) {
+            fs.readFile(__dirname + '/../../locales/markdown/' + path + 'en.md', 'utf8', function (err, data) {
+                if (err) {
+                    cb(err);
+                } else {
+                    parseMarkdown(data, cb);
+                }
+            });
+        } else {
+            parseMarkdown(data, cb);
+        }
+    });
+}
+
+function getTranslations(fileMap, language, callback) {
+    var tasks = {};
+    Object.keys(fileMap).forEach(function (e) {
+        tasks[e] = function (cb) {
+            getTranslatedMarkdown(fileMap[e], language, cb);
+        };
+    });
+
+    async.parallel(tasks, callback);
+}
+
+module.exports = {
+    getTranslations: getTranslations
+};
