5f0c166c1f6f0863c4eef444940cb0723d3330ce
Morten Høfft <mhoefft@gbif.org>
2016-10-17 14:03:50 +0200

    caveat on home page

--- app/controllers/index.ctrl.js
+++ app/controllers/index.ctrl.js
@@ -7,20 +7,23 @@ module.exports = function (app) {
     app.use('/', router);
 };
 
-router.get('/', function (req, res) {
-    //log.warn({randomAttribute: 'is test'}, 'My messagr grom the home controller');
-    //log.error({randomAttribute: 'ERROR'}, 'My ERROR MESSAGE GOES HERE');
+router.get('/', function (req, res, next) {
     if (typeof req.query.q !== 'undefined') {
         res.redirect(res.locals.gb.locales.urlPrefix + '/search?q=' + req.query.q);
     } else {
-        var articles = [new Article(), new Article()];
-        res.render('pages/home/home', {
-            title: 'Portal',
-            articles: articles,
-            message: 'shouldn\'t be here',
+        render(req, res, next, {
             _meta: {
+                title: 'GBIF',
                 hideSearchAction: true
             }
         });
     }
 });
+
+function render(req, res, next, data) {
+    try {
+        res.render('pages/home/home', data);
+    } catch (err) {
+        next(err);
+    }
+}
