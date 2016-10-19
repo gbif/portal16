c3d429593ebb7a920f4e7683368bca849155cd2b
Markus Döring <m.doering@mac.com>
2016-10-19 16:13:56 +0200

    Show parsed species name in heading with italics

--- app/controllers/species/key/speciesKey.ctrl.js
+++ app/controllers/species/key/speciesKey.ctrl.js
@@ -21,26 +21,29 @@ function taxonRoute(req, res, next) {
 function getTaxon(key, lang) {
     var deferred = Q.defer();
     var getOptions = {
-        expand: ['name','constituent','synonyms','combinations',
-            'occurrenceGeoRefCount','occurrenceCount',
-            'references','vernacular','typification']
+        // load these async through angular to respond quicker:
+        // 'synonyms','combinations','references','typification'
+        expand: ['name', 'constituent', 'occurrenceGeoRefCount', 'occurrenceCount', 'vernacular']
     };
 
     Taxon.get(key, getOptions).then(function (taxon) {
         // pick one vernacular name of requested language
         taxon.vernacular.results.some(function (v) {
             //TODO: create lookup map for 3 letter iso codes to 2 letter ones as used by the locale
+            // use http://api.gbif-dev.org/v1/enumeration/language
             if (v.language == lang || v.language=='eng') {
-                this.record.vernacularName = v.vernacularName;
-                this.record.vernacularLang = v.language;
-            }
-            return v.language == lang;
+                taxon.record.vernacularName = v.vernacularName;
+                taxon.record.vernacularLang = v.language;             }
+            return v.language == lang || v.language=='eng';
         });
         deferred.resolve(taxon);
 
     }, function (err) {
+        console.log(err);
         deferred.reject(new Error(err));
+
     }).fail(function (err) {
+        console.log(err);
         deferred.reject(new Error(err));
     }).done();
 
@@ -53,9 +56,10 @@ function renderPage(req, res, next, taxon) {
             res.json(taxon);
         } else {
             res.render('pages/species/key/speciesKey', {
+                t: taxon,
                 taxon: taxon,
                 _meta: {
-                    title: 'Taxon Detail ' + req.params.key
+                    title: taxon.record.scientificName
                 }
             });
         }
