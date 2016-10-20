a589bb718d6ee20fb62f01f5dbe1b9bbb67b3e4c
Burke Chih-Jen Ko <burkeker@gmail.com>
2016-10-20 13:02:08 +0200

    Guarding markdown rendering when there is no body object from CMS API.

--- app/controllers/cms/cmsContents.ctrl.js
+++ app/controllers/cms/cmsContents.ctrl.js
@@ -79,7 +79,6 @@ router.get([
                 // Render content if the result is not from URL Lookup.
                 if (typeof data.data[0] == 'object' && data.data[0].hasOwnProperty('created') && data.data[0].hasOwnProperty('title')) {
                     if (data.data[0].type == 'project' && data.data[0].projectId) {
-                        // @todo to wire the api endpoint when it goes in production.
                         return cmsData.cmsEndpointAccess('http://api.gbif-dev.org/v1/dataset/search?project_id=' + data.data[0].projectId).then(function (datasets) {
                             data.data[0].relatedDatasets = datasets.results;
                             renderPage(data, jsonOutput, next);
@@ -120,7 +119,12 @@ router.get([
                 // Expand/modify data before rendering
                 switch (body.data[0].type) {
                     case 'generic':
-                        body.data[0].body.markdown = md.render(body.data[0].body.value);
+                        if (body.data[0].body && body.data[0].body.value) {
+                            body.data[0].body.markdown = md.render(body.data[0].body.value);
+                        }
+                        else {
+                            body.data[0].body = {'markdown': '(No content provided yet.)'};
+                        }
                         break;
                 }
 
