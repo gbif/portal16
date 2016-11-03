5a6b0cb4196d9f0628786690bfd10eba907d9fba
Markus Döring <m.doering@mac.com>
2016-11-03 20:36:17 +0100

    show unevaluated label in case redlist and cites dont match anything

--- app/views/pages/species/key/directives/cites.directive.js
+++ app/views/pages/species/key/directives/cites.directive.js
@@ -11,7 +11,7 @@ angular
 function citesDirective() {
     var directive = {
         restrict: 'E',
-        template: '<a href="https://speciesplus.net/#/taxon_concepts/{{vm.id}}/legal" class="badge cites">{{vm.appendix}}</a>',
+        template: '<a href="{{vm.link}}" class="badge {{vm.class}}">{{vm.appendix}}</a>',
         scope: {},
         controller: citesCtrl,
         controllerAs: 'vm',
@@ -26,20 +26,28 @@ function citesDirective() {
         var vm = this;
         vm.appendix;
         vm.id;
+        vm.link;
+        vm.class;
         vm.listings=[];
 
         CitesApi.query({
             name: vm.name
 
         }, function (data) {
+            console.log(data);
             var tax = _.head(data.taxon_concepts);
             if (tax) {
                 vm.appendix = "Appendix " + tax.cites_listing;
                 vm.listings = tax.cites_listings;
                 vm.id = tax.id;
+                vm.class="cites";
+                vm.link = 'https://speciesplus.net/#/taxon_concepts/'+vm.id+'/legal'
+            } else {
+                vm.appendix = "Not listed";
+                vm.class="redlist-category-NE";
+                vm.link = '#'
             }
         }, function () {
-
         });
     }
 }
