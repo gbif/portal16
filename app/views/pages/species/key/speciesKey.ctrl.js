1f0dbeaecf6c36e445d3f4e4685769cd7204fcd0
Markus Döring <m.doering@mac.com>
2016-11-18 12:55:15 +0100

    Style nub species page taxonomy browser similar to dataset one

--- app/views/pages/species/key/speciesKey.ctrl.js
+++ app/views/pages/species/key/speciesKey.ctrl.js
@@ -6,7 +6,7 @@ require('./directives/redlist.directive.js');
 require('./directives/dbpedia.directive.js');
 require('./directives/synonyms.directive.js');
 require('./directives/combinations.directive.js');
-require('../../dataset/key/taxonomy/taxBrowser.directive.js');
+require('./directives/taxBrowser.directive.js');
 require('./directives/related.directive.js');
 require('./directives/references.directive.js');
 require('./directives/typeSpecimen.directive.js');
