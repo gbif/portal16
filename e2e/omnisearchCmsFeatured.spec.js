cdbd6ce2edf49f6fe0aff16e4e06e12eba471d57
Burke Chih-Jen Ko <burkeker@gmail.com>
2016-11-28 17:46:32 +0100

    e2e test to search BID programme shows as a featured item and inline image is correctly loaded.

---
+++ e2e/omnisearchCmsFeatured.spec.js
@@ -0,0 +1,18 @@
+'use strict';
+
+describe('Featured term search', function() {
+
+    it('BID should show BID programme as programme', function(){
+        browser.get('/');
+        element(by.css('#siteSearchInputHome')).sendKeys('bid');
+        element(by.css('.search-bar--home__search')).click();
+        expect(element.all(by.css('.article-card')).count()).toEqual(1);
+        expect(element(by.css('.card__content__title')).element(by.tagName('a')).getText()).toBe('BID: Biodiversity Information for Development');
+        element(by.css('.card__content__title')).element(by.tagName('a')).click();
+
+        var EC = protractor.ExpectedConditions;
+        var inlineImage = element(by.css('.prose-columns')).element(by.tagName('img'));
+        browser.wait(EC.presenceOf(inlineImage), 3000);
+        expect(inlineImage.getAttribute('complete')).toEqual('true');
+    });
+});
