60901b1670ee9530625a90afb05882196a44fc86
Burke Chih-Jen Ko <burkeker@gmail.com>
2016-11-30 12:55:09 +0100

    Lifted environment-dependant switch for regex.

--- app/helpers/cmsUrlEncode.js
+++ app/helpers/cmsUrlEncode.js
@@ -8,21 +8,7 @@ function appendImgCachePrefix(url) {
 }
 
 function extractAndEncodeUriMarkdown(text) {
-    let regex;
-    switch (env) {
-        case 'local':
-            regex = /\]\(http\:\/\/bko\.gbif\.org\/sites\/default\/files\/documents\/.*\.(png|PNG|jpg|JPG|jpeg|JPEG|pdf|PDF)/g;
-            break;
-        case 'dev':
-            regex = /\]\(http\:\/\/cms\.gbif-dev\.org\/sites\/default\/files\/documents\/.*\.(png|PNG|jpg|JPG|jpeg|JPEG|pdf|PDF)/g;
-            break;
-        case 'uat':
-            regex = /\]\(http\:\/\/cms\.gbif-uat\.org\/sites\/default\/files\/documents\/.*\.(png|PNG|jpg|JPG|jpeg|JPEG|pdf|PDF)/g;
-            break;
-        case 'prod':
-            regex = /\]\(http\:\/\/cms\.gbif-uat\.org\/sites\/default\/files\/documents\/.*\.(png|PNG|jpg|JPG|jpeg|JPEG|pdf|PDF)/g;
-            break;
-    }
+    let regex = /\]\(http\:\/\/([a-z]+\.)?gbif(-dev|-uat)?\.org\/sites\/default\/files\/documents\/.*\.(png|PNG|jpg|JPG|jpeg|JPEG|pdf|PDF)/g;
 
     return text.replace(regex, function (match) {
         return '](' + appendImgCachePrefix(encodeURIComponent(match.substring(2)));
@@ -31,27 +17,14 @@ function extractAndEncodeUriMarkdown(text) {
 
 // @todo merge in one function with extractAndEncodeUriMarkdown()
 function extractAndEncodeUriHtml(text) {
-    let regex;
-    switch (env) {
-        case 'local':
-            regex = /src\=\"http\:\/\/bko\.gbif\.org\/sites\/default\/files\/.*\.(png|PNG|jpg|JPG|jpeg|JPEG|pdf|PDF)/g;
-            break;
-        case 'dev':
-            regex = /src\=\"http\:\/\/cms\.gbif-dev\.org\/sites\/default\/files\/.*\.(png|PNG|jpg|JPG|jpeg|JPEG|pdf|PDF)/g;
-            break;
-        case 'uat':
-            regex = /src\=\"http\:\/\/cms\.gbif-uat\.org\/sites\/default\/files\/.*\.(png|PNG|jpg|JPG|jpeg|JPEG|pdf|PDF)/g;
-            break;
-        case 'prod':
-            regex = /src\=\"http\:\/\/cms\.gbif-uat\.org\/sites\/default\/files\/.*\.(png|PNG|jpg|JPG|jpeg|JPEG|pdf|PDF)/g;
-            break;
-    }
+    let regex = /src\=\"http\:\/\/([a-z]+\.)?gbif(-dev|-uat)?\.org\/sites\/default\/files\/.*\.(png|PNG|jpg|JPG|jpeg|JPEG|pdf|PDF)/g;
 
     return text.replace(regex, function (match) {
         return 'src="' + appendImgCachePrefix(encodeURIComponent(match.substring(5)));
     });
 }
 
+// @todo to be removed if it's not used.
 function processEncodedUrl(url) {
     let count = 0;
     while (count < 3 && url != decodeURIComponent(url)) {
