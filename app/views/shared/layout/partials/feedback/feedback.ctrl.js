176ef7f5e2c4eb1529267587c5a700e860270dd3
Morten Hofft <mortenhoefft@gmail.com>
2016-12-01 18:28:34 +0100

    feedback popup initiated

--- app/views/shared/layout/partials/feedback/feedback.ctrl.js
+++ app/views/shared/layout/partials/feedback/feedback.ctrl.js
@@ -12,6 +12,14 @@ angular
 function feedbackCtrl($scope, NAV_EVENTS) {
     var vm = this;
     vm.isActive = false;
+    vm.type = {
+        //0 left out to allow falsy test a la : if (vm.type) then ...
+        CONTENT: 1,
+        BUG: 2,
+        IDEA: 3,
+        QUESTION: 4
+    };
+    vm.selected = undefined;
 
     $scope.$on(NAV_EVENTS.toggleFeedback, function (event, data) {
         if (data.toggle) {
@@ -20,6 +28,18 @@ function feedbackCtrl($scope, NAV_EVENTS) {
             vm.isActive = data.state;
         }
     });
+
+    vm.close = function() {
+        vm.isActive = false;
+    }
+
+    vm.toggle = function(type) {
+        if (vm.selected == type) {
+            vm.selected = undefined;
+        } else {
+            vm.selected = type;
+        }
+    }
 }
 
 module.exports = feedbackCtrl;
