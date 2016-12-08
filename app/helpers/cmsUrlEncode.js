149aca842e82ea85ced9d473621c91c24e6a7e14
Burke Chih-Jen Ko <burkeker@gmail.com>
2016-12-08 17:05:12 +0100

    Excluded PDF from the image caching.

--- app/helpers/cmsUrlEncode.js
+++ app/helpers/cmsUrlEncode.js
@@ -7,7 +7,7 @@ function appendImgCachePrefix(url) {
 }
 
 function extractAndEncodeUriMarkdown(text) {
-    let regex = /\]\(http\:\/\/([a-z]+\.)?gbif(-dev|-uat)?\.org\/sites\/default\/files\/documents\/.*\.(png|PNG|jpg|JPG|jpeg|JPEG|pdf|PDF)/g;
+    let regex = /\]\(http\:\/\/([a-z]+\.)?gbif(-dev|-uat)?\.org\/sites\/default\/files\/documents\/.*\.(png|PNG|jpg|JPG|jpeg|JPEG)/g;
 
     return text.replace(regex, function (match) {
         return '](' + appendImgCachePrefix(encodeURIComponent(match.substring(2)));
@@ -16,7 +16,7 @@ function extractAndEncodeUriMarkdown(text) {
 
 // @todo merge in one function with extractAndEncodeUriMarkdown()
 function extractAndEncodeUriHtml(text) {
-    let regex = /src\=\"http\:\/\/([a-z]+\.)?gbif(-dev|-uat)?\.org\/sites\/default\/files\/.*\.(png|PNG|jpg|JPG|jpeg|JPEG|pdf|PDF)/g;
+    let regex = /src\=\"http\:\/\/([a-z]+\.)?gbif(-dev|-uat)?\.org\/sites\/default\/files\/.*\.(png|PNG|jpg|JPG|jpeg|JPEG)/g;
 
     return text.replace(regex, function (match) {
         return 'src="' + appendImgCachePrefix(encodeURIComponent(match.substring(5)));
