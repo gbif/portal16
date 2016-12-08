89196bb1303d1c705aa014a963c6b931d31e1169
Burke Chih-Jen Ko <burkeker@gmail.com>
2016-12-08 16:35:38 +0100

    Updated related projects validation test.

--- app/views/pages/about/programme/programme.ctrl.js
+++ app/views/pages/about/programme/programme.ctrl.js
@@ -1,16 +1,22 @@
 'use strict';
-var angular = require('angular');
+var angular = require('angular'),
+    programmeValidate = require('./programmeValidate');
 
 angular
     .module('portal')
     .controller('programmeCtrl', programmeCtrl);
 
 /** @ngInject */
-function programmeCtrl(CmsNode) {
+function programmeCtrl(CmsNode, $log) {
     var vm = this;
     vm.getNodeData = function () {
         CmsNode.get({type: vm.nodeType, id: vm.nodeId}).$promise.then(function (response) {
             vm.node = response.data[0];
+
+            // related project column integrity for BID
+            if (vm.node.id == '82243' && !programmeValidate(vm.node.relatedProjects)) {
+                $log.info('Projects lack grantType.')
+            }
         }, function (error) {
             return error;
         });
@@ -22,4 +28,3 @@ function programmeCtrl(CmsNode) {
 }
 
 module.exports = programmeCtrl;
-
---
+++ app/views/pages/about/programme/programmeValidate.js
@@ -0,0 +1,12 @@
+'use strict';
+
+// check if all the project has necessary columns so the table doesn't break.
+function relatedProjectsValidate(relatedProjects) {
+    var valid = true;
+    relatedProjects.forEach(function(project){
+        if (['small', 'national', 'regional'].indexOf(project.grantType) == -1) valid = false;
+    });
+    return valid;
+}
+
+module.exports = relatedProjectsValidate;
