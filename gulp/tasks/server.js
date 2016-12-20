fd76f3d505948654c27698bdce31acb0cb2241f1
Morten Høfft <mhoefft@gbif.org>
2016-12-20 15:25:38 +0100

    use configuration env when starting gulp en development mode

--- gulp/tasks/server.js
+++ gulp/tasks/server.js
@@ -42,7 +42,7 @@ gulp.task('server', ['server-lint'], function (cb) {
             tasks: ['server-lint'],
             watch: ['app.js', 'config/', 'app/'], //, 'app/views/**/*.nunjucks'
             env: {
-                'NODE_ENV': 'local'
+                'NODE_ENV': serverConfig.env
             },
             stdout: false
         })
