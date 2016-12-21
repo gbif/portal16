fd69e419f421daafbfa79b43f7383150aaf58257
Burke Chih-Jen Ko <burkeker@gmail.com>
2016-12-21 17:19:07 +0100

    Provided description for cmsUrlencode.spec.js.

--- app/helpers/cmsUrlEncode.spec.js
+++ app/helpers/cmsUrlEncode.spec.js
@@ -1,14 +1,25 @@
 'use strict';
 
+/**
+ * @fileoverview This test spec is written to test whether regex in cmsUrlEncode
+ * module can correctly convert inline image URLs into the prefixed version that
+ * is used by image cache, so that the image will show up correctly on the
+ * Express front end.
+ *
+ * The pattern assumes the text has been already escaped and come in from the
+ *  API endpoint (JSON), therefore the URLs already starts with
+ *  http://cms.gbif-{env}.org/.
+ */
+
 let cmsUrlEncode = require('./cmsUrlEncode'),
     apiConfig = require('../models/gbifdata/apiConfig'),
     mock = require('./cmsUrlEncode.mock.json');
 
-describe('CMS content formatting', function () {
-    it('Image URLs in markdown are converted correctly', function () {
+describe('TC_03_CMS_Inline_Image_Cache_Link', function () {
+    it('Inline image URLs in markdown are converted correctly', function () {
         expect(cmsUrlEncode.extractAndEncodeUriMarkdown(mock.markdownRaw)).toEqual(mock.markdownResult);
     });
-    it('Image URLs in HTML are converted correctly', function () {
+    it('Inline image URLs in HTML are converted correctly', function () {
         expect(cmsUrlEncode.extractAndEncodeUriHtml(mock.htmlRaw)).toEqual(mock.htmlResult);
     });
 });
