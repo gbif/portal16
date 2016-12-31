b05be63aea4898481dfc8e90f3ccf3ecbae2b158
Burke Chih-Jen Ko <burkeker@gmail.com>
2016-12-31 12:55:48 +0100

    Removed unused old version dataset details.

--- app/views/shared/layout/html/html.js
+++ app/views/shared/layout/html/html.js
@@ -109,9 +109,6 @@ require('../../../pages/tools/suggestDataset/suggestDataset.ctrl');
 require('../../../pages/dataset/key/datasetKey.ctrl');
 require('../../../pages/dataset/key/usage/datasetUsage.ctrl');
 
-require('../../../pages/dataset/details/datasetDetailsDownloadEventsTable.ctrl');
-require('../../../pages/dataset/details/datasetDetailsScrollSpy.ctrl');
-
 require('./angular/cmsFilter.service');
 require('../../../pages/cms/search/cms.ctrl');
 require('../../../pages/cms/search/table/cmsTable.ctrl');
