45802f4542ec7815b1af05531a5f8f1ef78cd995
Burke Chih-Jen Ko <burkeker@gmail.com>
2017-01-06 16:41:38 +0100

    Temporary code to dump non-participant countries.

--- app/controllers/api/directory/directoryParticipants.ctrl.js
+++ app/controllers/api/directory/directoryParticipants.ctrl.js
@@ -1,6 +1,10 @@
 'use strict';
 let express = require('express'),
     router = express.Router(),
+    jsonexport = require('jsonexport'),
+    fs = require('fs'),
+    helper = require('../../../models/util/util'),
+    dataApi = require('../../../models/gbifdata/apiConfig'),
     DirectoryParticipants = require('../../../models/gbifdata/directory/directoryParticipants'),
     log = require('../../../../config/log');
 
@@ -18,3 +22,39 @@ router.get('/directory/participants', (req, res, next) => {
             next(err)
         });
 });
+
+/*
+router.get('/directory/non-participants', (req, res, next) => {
+    DirectoryParticipants.groupBy()
+        .then(data => {
+            let options = {};
+            return helper.getApiDataPromise(dataApi.countryEnumeration.url, options)
+                .then(results => {
+                    let nonParticipants = results.filter(result => {
+                        let inArray = data.findIndex(p => {
+                            return p.countryCode == result.iso2;
+                        });
+                        return inArray === -1;
+                    });
+                    let exportOptions = {
+                    };
+                    //nonParticipants = JSON.stringify(nonParticipants);
+                    //nonParticipants = JSON.parse(nonParticipants);
+                    jsonexport(nonParticipants, exportOptions, (err, csv) => {
+                        if (err) return new Error(err.message);
+                        return fs.writeFile('/tmp/non-participants.csv', csv, (err) => {
+                            if (err) throw err;
+                            var message = 'Members retrieved and saved.';
+                            console.info(message);
+                            res.json({message: message});
+                            process.exit(0);
+                        });
+                    });
+                });
+        })
+        .catch(err => {
+            log.error('Error in /api/directory/participants controller: ' + err.message);
+            next(err)
+        });
+});
+*/
