c2b262ef67facbc74c908cf12db011bd5ab8a50b
Morten Høfft <mhoefft@gbif.org>
2017-01-16 16:41:09 +0100

    ternary filter

---
+++ app/views/components/filterTernary/filterTernary.directive.js
@@ -0,0 +1,60 @@
+'use strict';
+
+var angular = require('angular');
+angular
+    .module('portal')
+    .directive('filterTernary', filterTernaryDirective);
+
+/** @ngInject */
+function filterTernaryDirective() {
+    var directive = {
+        restrict: 'A',
+        transclude: true,
+        templateUrl: '/templates/components/filterTernary/filterTernary.html',
+        scope: {
+            filterState: '=',
+            filterConfig: '='
+        },
+        replace: true,
+        controller: filterTernary,
+        controllerAs: 'vm',
+        bindToController: true
+    };
+
+    return directive;
+
+    /** @ngInject */
+    function filterTernary($scope) {
+        var optionalBooleanRegEx = /(^true$)|(^false$)|^$/g;
+        var vm = this;
+        vm.queryKey = vm.filterConfig.queryKey;
+
+        //$scope.$watch(function () {
+        //    return vm.filterState.query[vm.queryKey]
+        //}, function (newQuery) {
+        //    updateQuery(newQuery);
+        //});
+
+        vm.change = function () {
+            vm.apply();
+        };
+
+        vm.apply = function () {
+            vm.filterConfig.filter.updateParam(vm.queryKey, vm.query);
+        };
+
+        function updateQuery(q) {
+            if (typeof q === 'string' && optionalBooleanRegEx.test(q)) {
+                vm.query = q;
+            } else {
+                vm.query = '';
+                vm.apply();
+            }
+        }
+        updateQuery(vm.filterState.query[vm.queryKey]);
+
+
+    }
+}
+
+module.exports = filterTernaryDirective;
--- app/views/shared/layout/html/html.js
+++ app/views/shared/layout/html/html.js
@@ -143,6 +143,7 @@ require('../../../components/filterFacet/filterFacet.directive');
 require('../../../components/count/count.directive');
 
 require('../../../components/filterEnum/filterEnum.directive');
+require('../../../components/filterTernary/filterTernary.directive');
 require('../../../components/filterCms/filterCms.directive');
 
 require('../../../components/focusMe/focusMe.directive');
