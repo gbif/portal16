cd256faa934a2c578239847872dfd14c51da6572
Morten Høfft <mhoefft@gbif.org>
2017-01-17 17:53:23 +0100

    occ search - date selector filters added. minor usability issues remain

--- app/views/components/filterDate/parseDateIntervalQuery.js
+++ app/views/components/filterDate/parseDateIntervalQuery.js
@@ -1,22 +1,25 @@
 "use strict";
 var parseIntervalQuery = require('../filterInterval/parseIntervalQuery'),
-    reg = /^\d{1,4}\-((0[1-9])|1[0-2])$/;
+    reg = /^\d{1,4}\-((0?[1-9])|1[0-2])$/;
 module.exports = function (q) {
     var interval = parseIntervalQuery(q);
     if (!interval) {
         return invalidResponse();
     }
 
-    if (!isDate(interval.values[0])) {
-        return invalidResponse();
-    } else {
-        interval.values[0] = parseDate(interval.values[0]);
+    if (interval.type === 'between' || interval.type === 'largerThan' || interval.type === 'is') {
+        if (!isDate(interval.values[0])) {
+            return invalidResponse();
+        } else {
+            interval.from = parseDate(interval.values[0]);
+        }
     }
-
-    if (!isDate(interval.values[1])) {
-        return invalidResponse();
-    } else {
-        interval.values[1] = parseDate(interval.values[1]);
+    if (interval.type === 'between' || interval.type === 'lessThan') {
+        if (!isDate(interval.values[1])) {
+            return invalidResponse();
+        } else {
+            interval.to = parseDate(interval.values[1]);
+        }
     }
     return interval;
 };
@@ -25,16 +28,12 @@ function parseDate(str) {
     var parts = str.split('-');
     return {
         year: parseInt(parts[0]),
-        month: parseInt(parts[1])
+        month: parseInt(parts[1]).toString()
     };
 }
 
 function isDate(num) {
-    if (!num) {
-        return false;
-    } else {
-        return reg.test(num);
-    }
+    return reg.test(num);
 }
 
 function invalidResponse(){
