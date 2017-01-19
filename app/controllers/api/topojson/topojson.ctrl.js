a9280ca90f36ea78bff45f17ccfc62161ed77496
Burke Chih-Jen Ko <burkeker@gmail.com>
2017-01-19 17:18:20 +0100

    Colored map.

--- app/controllers/api/topojson/topojson.ctrl.js
+++ app/controllers/api/topojson/topojson.ctrl.js
@@ -5,9 +5,11 @@
  */
 
 const express = require('express'),
-    router = express.Router(),
-    worldRobinson = require('./world-robinson-topo.json');
-    //apicache = require('apicache');
+      router = express.Router(),
+      helper = require('../../../models/util/util'),
+      topojson = require('topojson'),
+      worldRobinson = require('./c-robinson-quantized-topo.json');
+      //apicache = require('apicache');
 
 //let cache = apicache.middleware;
 
@@ -16,5 +18,28 @@ module.exports = app => {
 };
 
 router.get('/topojson/world-robinson', (req, res, next) => {
-    res.json(worldRobinson);
+    let options = {'qs': {'gbifRegion':'GLOBAL'}},
+        url = 'http://' + req.get('host') + '/api/directory/participants/active';
+
+    helper.getApiDataPromise(url, options)
+        .then(participants => {
+
+            // decorate each feature if an active participant is associated with it.
+            let keyed = {};
+            participants.forEach(p => {
+                // only country participant will be shown on map.
+                if (p.type !== 'OTHER') {
+                    keyed[p.countryCode] = p;
+                }
+            });
+            worldRobinson.objects.tracts.geometries.forEach(geo => {
+                if (keyed[geo.properties.ISO2]) {
+                    geo.properties = Object.assign(geo.properties, keyed[geo.properties.ISO2]);
+                }
+            });
+            res.json(worldRobinson);
+        })
+        .catch(e => {
+            next(e);
+        });
 });
--- app/views/pages/theGbifNetwork/theGbifNetworkMap.directive.js
+++ app/views/pages/theGbifNetwork/theGbifNetworkMap.directive.js
@@ -20,6 +20,11 @@ function theGbifNetworkMap() {
     };
 
     function drawMap(scope, element, attrs) {
+        var color = {
+            'voting_participant': '#4E9F37',
+            'associate_country_participant': '#58BAE9'
+        };
+
         var margin = {top: 40, right: 20, bottom: 50, left: 40},
             width = 960 - margin.left - margin.right,
             height = 480 - margin.top - margin.bottom;
@@ -37,8 +42,13 @@ function theGbifNetworkMap() {
                 .selectAll("path")
                 .data(topojson.feature(topology, topology.objects.tracts).features)
                 .enter().append("path")
-                .attr("d", path);
-
+                .attr("d", path)
+                .attr("fill", function(d){
+                    if (d.properties.hasOwnProperty('membershipType')) {
+                        return color[d.properties.membershipType];
+                    }
+                    return '#DFDDCF';
+                });
         });
     }
 }
