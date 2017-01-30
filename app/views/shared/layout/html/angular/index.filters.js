ca1aba608a4dde048e9cb72bae02214b16400024
Morten Høfft <mhoefft@gbif.org>
2017-01-30 17:30:27 +0100

    angular filter for formating bytes askb, mb, gb etc

--- app/views/shared/layout/html/angular/index.filters.js
+++ app/views/shared/layout/html/angular/index.filters.js
@@ -80,4 +80,15 @@
                 return (text) ? text.charAt(0).toUpperCase() + '.' : '';
             }
         })
+        .filter('formatBytes', function(){
+            return function(bytes, lang, decimals) {
+                decimals = decimals || 1;
+                if (bytes == 0) return '0 Byte';
+                var k = 1000;
+                var dm = decimals + 1 || 3;
+                var sizes = ['Bytes', 'KB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB'];
+                var i = Math.floor(Math.log(bytes) / Math.log(k));
+                return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)).toLocaleString(lang) + ' ' + sizes[i];
+            }
+        })
 })();
