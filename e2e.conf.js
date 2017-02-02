6adaad5a2c03970402209de78b4fa4982b1c60bd
Burke Chih-Jen Ko <burkeker@gmail.com>
2017-02-02 20:29:26 +0100

    e2e test for single select.

--- e2e.conf.js
+++ e2e.conf.js
@@ -1,6 +1,6 @@
 exports.config = {
     seleniumAddress: 'http://localhost:4444/wd/hub',
-    baseUrl: 'https://demo.gbif-dev.org',
+    baseUrl: 'http://demo.gbif-dev.org',
     allScriptsTimeout: 80000,
     specs: ['e2e/*.spec.js']
 };
---
+++ e2e/po/cmsSearch.po.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var cmsSearch = function () {
+    this.url = '/cms/search';
+    this.facetPurpose = element(by.id('category_purpose'));
+    this.purposeFilters = this.facetPurpose.all(by.css('.checkbox'));
+};
+module.exports = new cmsSearch();
