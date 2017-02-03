892440804a2bc4f15b0a68ed774716cac6b86ed1
Burke Chih-Jen Ko <burkeker@gmail.com>
2017-02-03 12:23:43 +0100

    Fixed inconsistency of param ordering which caused 500 error on analytics/global.

--- app/controllers/analytics/analytics.ctrl.js
+++ app/controllers/analytics/analytics.ctrl.js
@@ -18,11 +18,11 @@ router.get('/country/:country/about', function (req, res, next) {
 
 router.get('/country/:country/published', function (req, res, next) {
     //TODO: make sure publishing country exists in GBIF
-    renderPage(res, next, 'country/' + req.params.country + '/publishedBy', req.params.country, false);
+    renderPage(req, res, next, 'country/' + req.params.country + '/publishedBy', req.params.country, false);
 });
 
 
-function renderPage(res, next, path, country, about) {
+function renderPage(req, res, next, path, country, about) {
     try {
         res.render('pages/analytics/analytics', {
             country: country,
