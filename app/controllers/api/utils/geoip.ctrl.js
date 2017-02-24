b08366398fed77ac429d402e866b44e5f70a1fec
Morten Høfft <mhoefft@gbif.org>
2017-02-24 17:56:40 +0100

    geoip endpoint for country only

--- app/controllers/api/utils/geoip.ctrl.js
+++ app/controllers/api/utils/geoip.ctrl.js
@@ -2,6 +2,9 @@
 let express = require('express'),
     router = express.Router(),
     log = rootRequire('config/log'),
+    minute = 60000,
+    hour =  60*minute,
+    _ = require('lodash'),
     getGeoIp = rootRequire('app/helpers/utils').getGeoIp;
 
 module.exports = function (app) {
@@ -13,6 +16,27 @@ router.get('/geoip', function (req, res) {
         country = getGeoIp(ip);
     if (!country) {
         log.error('unable to match ip to country using ip: ' + ip);
+        res.setHeader('Cache-Control', 'no-cache');
+        res.status(404);
+        res.send();
+    } else {
+        res.setHeader('Cache-Control', 'private, max-age=' + hour);
+        res.json({country: country});
     }
-    res.json({country: country});
+});
+
+router.get('/geoip/country', function (req, res) {
+    let ip = req.clientIp,
+        country = getGeoIp(ip),
+        countryCode = _.get(country, 'country.iso_code');
+
+    if (!countryCode) {
+        log.error('unable to match ip to country using ip: ' + ip);
+        res.setHeader('Cache-Control', 'no-cache');
+        res.status(404);
+        res.send();
+    } else {
+        res.setHeader('Cache-Control', 'private, max-age=' + hour);
+        res.send(countryCode);
+    }
 });
