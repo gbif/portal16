908a634dc06b95467739d195d422b725763efe18
Morten Høfft <mhoefft@gbif.org>
2017-06-08 17:58:06 +0200

    element query directive and mapwidget style for mobile

---
+++ app/views/components/elementQuery/elementQuery.directive.js
@@ -0,0 +1,34 @@
+'use strict';
+var angular = require('angular'),
+    _ = require('lodash');
+
+angular
+    .module('portal')
+    .directive('elementQuery', function ($window) {
+        return {
+            restrict: 'A',
+            scope: {
+                elementQuery: '='
+            },
+            link: function (scope, element) { //attrs
+                var length = scope.elementQuery.breakpoints.length;
+                function updateSize() {
+                    var largestIndex = 0,
+                        width = element[0].clientWidth;
+                    for (var i = 0; i < length; i++) {
+                        element.removeClass(scope.elementQuery.classes[i]);
+                        if (width > scope.elementQuery.breakpoints[i]) {
+                            largestIndex = i;
+                        }
+                    }
+                    element.addClass(scope.elementQuery.classes[largestIndex]);
+                    scope.elementQuery.current = largestIndex;
+                }
+                updateSize();
+                angular.element($window).bind('resize', function(){
+                    updateSize();
+                });
+            }
+        };
+    });
+
