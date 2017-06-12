c7b4f08d3d94992c321c9ff232f6701499191f0e
Morten Høfft <mhoefft@gbif.org>
2017-06-12 09:12:53 +0200

    update endpoint config to use prod when available

--- app/views/shared/layout/html/angular/map.resource.js
+++ app/views/shared/layout/html/angular/map.resource.js
@@ -18,7 +18,7 @@ var angular = require('angular');
             );
         })
         .factory('MapCapabilities', function ($resource, env) {
-            return $resource('//api.gbif-dev.org/v2/map/occurrence/density/capabilities.json', null, {
+            return $resource(env.dataApiV2 + 'map/occurrence/density/capabilities.json', null, {
                     'query': {
                         method: 'GET',
                         isArray: false
@@ -26,15 +26,5 @@ var angular = require('angular');
                 }
             );
         })
-        .factory('OccurrenceBbox', function ($resource) {
-            return $resource('//cdn.gbif.org/v1/map/density/tile.json?resolution=1&x=0&y=0&z=0', null, {
-                    'query': {
-                        method: 'GET',
-                        isArray: false
-                    }
-                }
-            );
-        });
-
 })();
 
