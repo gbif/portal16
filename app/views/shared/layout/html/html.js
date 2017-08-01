87617f6c038af8bbafe6166da85a7dd347cdba44
Morten Høfft <mhoefft@gbif.org>
2017-08-01 17:02:37 +0200

    move terms into directive for performance and so it wont show in google results

--- app/views/shared/layout/html/html.js
+++ app/views/shared/layout/html/html.js
@@ -259,7 +259,7 @@ require('../partials/notifications/notifications.directive');
 
 require('./angular/nav.constants');
 require('../partials/navigation/nav.ctrl');
-require('../partials/popups/terms/terms.ctrl');
+require('../partials/popups/terms/terms.directive');
 
 
 require('../../../pages/search/search.ctrl');
