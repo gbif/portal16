897ec84421b6c7190ec138b1b2c7acc731b25739
Morten Hofft <mortenhoefft@gmail.com>
2017-08-03 20:25:17 +0200

    participant dataset list of by one error fixed

---
+++ app/views/components/modal/modal.directive.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var angular = require('angular');
+angular
+    .module('portal')
+    .directive('gbModal', modalDirective);
+
+/** @ngInject */
+function modalDirective(BUILD_VERSION) {
+    var directive = {
+        restrict: 'A',
+        transclude: true,
+        templateUrl: '/templates/components/modal/modal.html?v=' + BUILD_VERSION,
+        scope: {
+            onDismiss: '&'
+        },
+        replace: true,
+        controller: modal,
+        controllerAs: 'vm',
+        bindToController: true
+    };
+
+    return directive;
+
+    /** @ngInject */
+    function modal($timeout) {
+        var vm = this;
+        $timeout(function(){vm.fadeIn = true;}, 0);
+        vm.dismiss = function(){
+            if (typeof vm.onDismiss == 'function') {
+                vm.fadeIn = false;
+                vm.onDismiss();
+            }
+        }
+    }
+}
+
+module.exports = modalDirective;
+
