353e479590775600bc7ff4ee300ddf38d4f7f06b
Morten Høfft <mhoefft@gbif.org>
2017-08-04 09:58:43 +0200

    add seo version of individual directory contacts

--- app/controllers/api/directory/directory.model.js
+++ app/controllers/api/directory/directory.model.js
@@ -2,9 +2,7 @@
 
 let apiConfig = rootRequire('app/models/gbifdata/apiConfig'),
     chai = require('chai'),
-    expect = chai.expect,
     querystring = require('querystring'),
-    request = require('requestretry'),
     _ = require('lodash'),
     authOperations = require('../../auth/gbifAuthRequest');
 
--- app/views/shared/layout/html/angular/routerConfig.js
+++ app/views/shared/layout/html/angular/routerConfig.js
@@ -276,7 +276,7 @@ function routerConfig($stateProvider, $locationProvider, BUILD_VERSION) {
         })
         .state('contactUs', {
             parent: 'localization',
-            url: '/contact-us2',
+            url: '/contact-us',
             views: {
                 main: {
                     templateUrl: '/api/template/contactUs/contactUs.html?v=' + BUILD_VERSION,
