1c4b9b5519753a0ef80dedb7a4a69eae22ac17a9
Morten Høfft <mhoefft@gbif.org>
2017-08-09 15:00:24 +0200

    make country suggest json align with API usage

--- app/controllers/api/country/suggest/countrySuggest.ctrl.js
+++ app/controllers/api/country/suggest/countrySuggest.ctrl.js
@@ -4,6 +4,7 @@
  */
 var express = require('express'),
     router = express.Router(),
+    countryCodes = rootRequire('app/models/enums/basic/country'),
     en = require('../../../../../locales/server/en.json'),
     da = require('../../../../../locales/server/da.json');
 
@@ -13,23 +14,27 @@ module.exports = function (app) {
 
 //TODO make gneric one that uses locales to fetch and if not present fallback to english
 router.get('/country/suggest.json', function (req, res) {
-    var lang = req.query.lang;
-    var countrySuggestions = [];
-    if (lang == 'en') {
-        countrySuggestions = getSuggestions(en.country);
-    } else if (lang == 'da') {
-        countrySuggestions = getSuggestions(da.country);
-    } else {
-        countrySuggestions = getSuggestions(en.country);
-    }
-    res.json(countrySuggestions);
+    //var lang = req.query.lang;
+    //var countrySuggestions = [];
+    //if (lang == 'en') {
+    //    countrySuggestions = getSuggestions(en.country);
+    //} else if (lang == 'da') {
+    //    countrySuggestions = getSuggestions(da.country);
+    //} else {
+    //    countrySuggestions = getSuggestions(en.country);
+    //}
+    //res.json(countrySuggestions);
+    let countries = countryCodes.map(function(key){
+        return {key: key, title: req.__('country.' + key)};
+    });
+    res.json(countries);
 });
 
-function getSuggestions(translations) {
-    return Object.keys(translations).map(function (countryCode) {
-        return {
-            key: countryCode,
-            title: translations[countryCode]
-        };
-    });
-}
+//function getSuggestions(translations) {
+//    return Object.keys(translations).map(function (countryCode) {
+//        return {
+//            key: countryCode,
+//            title: translations[countryCode]
+//        };
+//    });
+//}
