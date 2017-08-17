9fbc2dd5d2e09e7fed3b8cbe1da20cb3b2d123a3
Morten Høfft <mhoefft@gbif.org>
2017-08-17 09:41:06 +0200

    enable login and registration

--- app/views/pages/user/profile/userProfile.ctrl.js
+++ app/views/pages/user/profile/userProfile.ctrl.js
@@ -10,7 +10,7 @@ angular
 /** @ngInject */
 function userProfileCtrl(User, BUILD_VERSION, LOCALE, regexPatterns, $http, toastService) {
     var vm = this;
-    vm.disableEditing = true;
+    vm.disableEditing = false;
     vm.emailPattern = regexPatterns.email;
 
     vm.getUser = function() {
