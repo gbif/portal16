8030025ac3c9d6d3c8bcb6f39868222758514be1
Morten Høfft <mhoefft@gbif.org>
2017-08-18 17:48:33 +0200

    js failure when fullscreen not defined in widget

---
+++ app/views/pages/participant/country/research/countryResearch.ctrl.js
@@ -0,0 +1,21 @@
+'use strict';
+
+var angular = require('angular');
+
+angular
+    .module('portal')
+    .controller('countryAboutCtrl', countryAboutCtrl);
+
+/** @ngInject */
+function countryAboutCtrl($stateParams, OccurrenceTableSearch, OccurrenceDatasetSearch) {
+    var vm = this;
+    vm.countryCode = $stateParams.key;
+
+    OccurrenceDatasetSearch.query({country: vm.countryCode}, function (data) {
+        vm.datasetsAbout = data;
+    }, function () {
+        //TODO handle request error
+    });
+}
+
+module.exports = countryAboutCtrl;
