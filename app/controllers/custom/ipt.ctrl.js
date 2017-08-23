26455732cb9c6a77412f016a265c9022d7b62dc8
Morten Høfft <mhoefft@gbif.org>
2017-08-23 14:56:13 +0200

    move ipt to custom route folder

---
+++ app/controllers/custom/ipt.ctrl.js
@@ -0,0 +1,28 @@
+"use strict";
+var express = require('express'),
+    router = express.Router(),
+    resource = rootRequire('app/controllers/resource/key/resourceKey'),
+    helper = rootRequire('app/models/util/util');
+
+module.exports = function (app) {
+    app.use('/', router);
+};
+
+router.get('/ipt\.:ext?', function (req, res, next) {
+
+    let iptPage = resource.getByAlias('/ipt', 2, false, res.locals.gb.locales.current);
+    iptPage.then(
+        function (data) {
+            helper.renderPage(req, res, next, {
+                prose: data,
+                _meta: {
+                    title: 'IPT',
+                    description: req.__("meta.iptDescription")
+                }
+            }, 'pages/custom/ipt/ipt');
+        },
+        function (err) {
+            next(err);
+        }
+    );
+});
