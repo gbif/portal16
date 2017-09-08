583213cd5ceb2be76a049110835b3afe53505c5e
Thomas Stjernegaard <tsjeppesen@gbif.org>
2017-09-08 09:47:21 +0200

    closes #548

--- app/middleware/redirects/redirects.js
+++ app/middleware/redirects/redirects.js
@@ -12,25 +12,37 @@ redirectList.forEach(function(e){
 });
 
 function handleRedirects(req, res, next) {
-
-    let redirectTo = redirectTable[req.path];
+    let redirectTo = redirectTable[req.url];
     if (!redirectTo && ignoreTrailingSlashes ) {
-        redirectTo = redirectTable[removeSlashes(req.path)];
+        redirectTo = redirectTable[addTrailingSlash(req.url)];
     }
 
     if (redirectTo) {
-        let q = '';
-        if (!_.isEmpty(req.query)) {
-            q = '?' + querystring.stringify(req.query);
-        }
-        res.redirect(302, redirectTo + q);
+        // let q = '';
+        // if (!_.isEmpty(req.query)) {
+        //     q = '?' + querystring.stringify(req.query);
+        // }
+        res.redirect(302, redirectTo);
     } else {
         next();
     }
 }
 
 function removeSlashes(str){
-    return str.replace(/(\/)+$/,'');
+
+    let parts = str.split("?");
+    let path = parts[0];
+
+    let query = (parts[1] !== undefined) ? "?"+parts[1] : "";
+    return path.replace(/(\/)+$/,'') + query;
+}
+
+function addTrailingSlash(str){
+
+    let parts = str.split("?");
+    let path = parts[0];
+    let query = (parts[1] !== undefined) ? "?"+parts[1] : "";
+    return path+"/" + query;
 }
 
 
