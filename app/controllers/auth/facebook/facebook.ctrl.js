7d1951770faa4aa7e49f1085900f2ce9a835ffae
Morten Høfft <mhoefft@gbif.org>
2017-10-12 18:02:06 +0200

    start preparing facebook login

--- app/controllers/auth/facebook/facebook.ctrl.js
+++ app/controllers/auth/facebook/facebook.ctrl.js
@@ -2,44 +2,49 @@
 let express = require('express'),
     router = express.Router(),
     passport = require('passport'),
-    //credentials = rootRequire('config/credentials').auth.facebook,
-    //User = require('../../api/user/user.model'),
-    //auth = require('../auth.service'),
+//credentials = rootRequire('config/credentials').auth.facebook,
+//User = require('../../api/user/user.model'),
+//auth = require('../auth.service'),
     FacebookStrategy = require('passport-facebook').Strategy;
 
+let clientId = 'nonse',
+    clientSecret = 'nonse',
+    callbackURL = 'http://localhost:7000/auth/facebook/callback',
+    profileFields = ['id', 'email', 'displayName', 'picture'];
+
+let facebookConfig = {
+    // pull in our app id and secret from our auth.js file
+    clientID: clientId,
+    clientSecret: clientSecret,
+    callbackURL: callbackURL,
+    profileFields: profileFields
+};
 
 module.exports = function (app) {
     app.use('/auth', router);
 };
 
-router.get('/facebook', passport.authenticate('facebook', {scope: 'email'}));
+router.get('/facebook/connect', function (req, res, next) {
+    //perhaps state should be a base64 encoded json. target and action (login|create|connect) ?
+    // on the other hand if already logged in, then always connect. if not logged in, then log in or create if non existent (providing option to select username)
+    passport.authenticate('facebook', {scope: 'email', state: req.query.state})(req, res, next);
+});
 
 router.get('/facebook/callback', function (req, res, next) {
     passport.authenticate('facebook', {session: false}, function (err, user, info) {
         //TODO to implement - only to see that it all works and we can get info from orcID
         //TODO send to page with option to select username and email and optional password. How to store the userid? encode it as a temporary token?
-        res.json({err, user, info});
+        //console.log(req.query.state);
+        //console.log(user);
+        //TODO update the user info here based on the returned user
+        res.redirect(302, req.query.state);
     })(req, res, next);
 });
 
-let clientId = 'clientId',
-    clientSecret = 'clientSecret',
-    callbackURL = 'http://localhost:7000/auth/facebook/callback',
-    profileFields = ['id', 'email', 'displayName', 'picture'];
-
-passport.use(new FacebookStrategy({
-
-        // pull in our app id and secret from our auth.js file
-        clientID: clientId,
-        clientSecret: clientSecret,
-        callbackURL: callbackURL,
-        profileFields: profileFields
-
-    },
+passport.use(new FacebookStrategy(facebookConfig,
 
     // facebook will send back the token and profile
     function (token, refreshToken, profile, done) {
-        //TODO to implement - only to see that it all works and we can get info from orcID
-        done({token, refreshToken, profile});
+        done(null, {token, refreshToken, profile}, {nothing: 'to say'});
     })
 );
