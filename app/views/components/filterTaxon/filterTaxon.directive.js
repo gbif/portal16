ca756f5d2378d0673a2833bc41a0a4a19532bce5
Morten Høfft <mhoefft@gbif.org>
2018-01-10 15:34:53 +0100

    fallback on taxon browser for unranked occurrences

--- app/views/components/filterTaxon/filterTaxon.directive.js
+++ app/views/components/filterTaxon/filterTaxon.directive.js
@@ -50,8 +50,6 @@ function filterTaxonDirective(BUILD_VERSION) {
         $scope.$watchCollection(function () {
             return vm.filterState.query
         }, function (newState, oldState) {
-            console.log('update');
-            console.log(newState);
             if (vm.filterConfig.facets && vm.filterConfig.facets.hasFacets && !angular.equals(newState, oldState)) {
                 vm.setFacetSuggestions();
             }
--- app/views/components/occurrenceTaxonomy/occurrenceTaxonomy.directive.js
+++ app/views/components/occurrenceTaxonomy/occurrenceTaxonomy.directive.js
@@ -34,9 +34,9 @@ function occurrenceTaxonomyDirective(BUILD_VERSION) {
     }
 
     /** @ngInject */
-    function occurrenceTaxonomy($scope, $state, OccurrenceFrequentTaxa, OccurrenceFilter, OccurrenceTaxonSearch) {
+    function occurrenceTaxonomy($q, Species, $scope, $state, OccurrenceFrequentTaxa, OccurrenceFilter, OccurrenceTaxonSearch) {
         var vm = this;
-        vm.fullTree = {children:[]};
+        vm.fullTree = {children:[], expanded: true};
 
         function updateTree() {
             if (_.get(vm.frequent, '$cancelRequest')) {
@@ -57,23 +57,80 @@ function occurrenceTaxonomyDirective(BUILD_VERSION) {
         }
 
         var ranks = ['kingdom', 'phylum', 'class', 'order', 'family', 'genus', 'species'];
-        vm.appendTaxa = function (item) {
-            if (item.children.length > 0) {
+        vm.toggleTaxa = function (item) {
+            if (item.expanded) {
                 item.children = [];
                 item.expanded = false;
-                return;
+            } else {
+                vm.appendTaxa(item);
             }
-            item.expanded = true;
-            var filter = vm.filter || {};
-            var customQuery = {offset: item.children.length, limit: 20, taxonKey: item.key};
-            var rank = item.rank || '';
+        };
+
+        function asArray(e) {
+            if (_.isArray(e)) {
+                return e;
+            } else if(_.isUndefined(e)) {
+                return [];
+            } else {
+                return [e];
+            }
+        }
+
+        vm.nextRank = function(rank){
+            if (!rank) {
+                return 'kingdom';
+            }
             var nextRank = ranks[ranks.indexOf(rank.toLowerCase()) + 1];
+            return nextRank;
+        };
+
+        function getRankId(rank) {
+            if (!rank) {
+                rank = '';
+            }
+            return ranks.indexOf(rank.toLowerCase());
+        }
+
+        function hasTaxonKey(taxon, key, rank) {
+            return taxon[rank.toLowerCase() + 'Key'] === key;
+        }
+
+        vm.appendTaxa = function (item) {
+            item.expanded = true;
+            item.state = 'LOADING';
+            var filter = vm.filter || {};
+            //what taxonkey to filter on? this is tricky as there could be an existing filter on one or more taxa. Depending on what node you expand, the existing filter would have to be included partially, fully or not at all.
+
+            /*
+            hvis key i en af eksisterende filters parents:
+                fjern alle eksisterende filters der ikke har denne key
+            ellers er det et dybere liggende barn eller eks eksakt key
+                brug kun denne key
+             */
+
+             var customQuery = {offset: item.children.length, limit: 20};
+             if (item.key) {
+                 var filterKeys = [];
+                 vm.filteredTaxa.forEach(function (taxon) {
+                     if (hasTaxonKey(taxon, item.key, item.rank)) {
+                         filterKeys.push(taxon.key);
+                     }
+                 });
+                 if (filterKeys.length == 0 && item.key) {
+                     filterKeys.push(item.key);
+                 }
+                 customQuery.taxon_key = filterKeys;
+            }
+
+            var rank = item.rank || '';
+            var nextRank = vm.nextRank(rank);
             if (!nextRank) {
-                item.loading = false;
+                item.state = undefined;
+                item.loading = 'END';
                 return;
             }
             customQuery.type = nextRank;
-            var query = _.merge({}, filter, customQuery);
+            var query = _.assign({}, filter, customQuery);
             item.loading = true;
             OccurrenceTaxonSearch.query(query, function (data) {
                 //item.children = [{scientificName: '234', children: []}, {scientificName: '234', children: []}];
@@ -81,18 +138,36 @@ function occurrenceTaxonomyDirective(BUILD_VERSION) {
                     e.children = [];
                 });
                 item.children = _.concat(item.children, data.results);
+                item.childCount = _.sumBy(item.children, '_count');
                 if (item.children.length)
                 //item.children.splice(-1, 0, data.results);
                 //item.children.push({scientificName: '234', children: []});
                 item.endOfRecords = data.endOfRecords;
+                item.state = data.endOfRecords ? 'END' : 'MORE';
                 item.loading = false;
+                if (item.children.length == 1 && data.endOfRecords) {
+                    vm.toggleTaxa(item.children[0])
+                }
             }, function () {
                 //TODO handle request error
             });
         };
+
         function restartTree() {
             vm.fullTree = {children:[]};
-            vm.appendTaxa(vm.fullTree);
+            //Get taxa that is a part of the filter
+            var filteredTaxaPromises = asArray(vm.filter.taxon_key).map(function(e){
+                return Species.get({id: e});
+            });
+            vm.filteredTaxa = [];
+            $q.all(filteredTaxaPromises)
+                .then(function(results){
+                    vm.filteredTaxa = results;
+                    vm.appendTaxa(vm.fullTree);
+                })
+                .catch(function(err){
+                    console.log(err);
+                });
         }
         restartTree();
 
@@ -128,5 +203,3 @@ function occurrenceTaxonomyDirective(BUILD_VERSION) {
 }
 
 module.exports = occurrenceTaxonomyDirective;
-
-var tester = {"rank":"kingdom","children":[{"key":1,"nubKey":1,"nameKey":13352168,"taxonID":"gbif:1","kingdom":"Animalia","kingdomKey":1,"datasetKey":"d7dddbf4-2cf0-4f39-9b2a-bb099caae36c","constituentKey":"d7dddbf4-2cf0-4f39-9b2a-bb099caae36c","scientificName":"Animalia","canonicalName":"Animalia","authorship":"","nameType":"SCIENTIFIC","rank":"KINGDOM","origin":"SOURCE","taxonomicStatus":"ACCEPTED","nomenclaturalStatus":[],"remarks":"","publishedIn":"Linnæus, Carolus.  1758. Systema naturae per regna tria naturae, secundum classes, ordines, genera, species, cum characteribus, differentiis, synonymis, locis. Laurentii Salvii, Holmiae [= Stockholm].  Vol. Tomus I,  Editio decima, reformata: i-ii, 1-824.","numDescendants":2323575,"lastCrawled":"2017-02-14T13:42:21.926+0000","lastInterpreted":"2017-02-14T12:05:57.974+0000","issues":[],"synonym":false,"_count":6331186,"$$hashKey":"object:130","children":[{"key":1,"nubKey":1,"nameKey":13352168,"taxonID":"gbif:1","kingdom":"Animalia","kingdomKey":1,"datasetKey":"d7dddbf4-2cf0-4f39-9b2a-bb099caae36c","constituentKey":"d7dddbf4-2cf0-4f39-9b2a-bb099caae36c","scientificName":"Animalia","canonicalName":"Animalia","authorship":"","nameType":"SCIENTIFIC","rank":"KINGDOM","origin":"SOURCE","taxonomicStatus":"ACCEPTED","nomenclaturalStatus":[],"remarks":"","publishedIn":"Linnæus, Carolus.  1758. Systema naturae per regna tria naturae, secundum classes, ordines, genera, species, cum characteribus, differentiis, synonymis, locis. Laurentii Salvii, Holmiae [= Stockholm].  Vol. Tomus I,  Editio decima, reformata: i-ii, 1-824.","numDescendants":2323575,"lastCrawled":"2017-02-14T13:42:21.926+0000","lastInterpreted":"2017-02-14T12:05:57.974+0000","issues":[],"synonym":false,"_count":6331186}],"endOfRecords":true},{"key":6,"nubKey":6,"nameKey":13566757,"taxonID":"gbif:6","kingdom":"Plantae","kingdomKey":6,"datasetKey":"d7dddbf4-2cf0-4f39-9b2a-bb099caae36c","constituentKey":"d7dddbf4-2cf0-4f39-9b2a-bb099caae36c","scientificName":"Plantae","canonicalName":"Plantae","authorship":"","nameType":"SCIENTIFIC","rank":"KINGDOM","origin":"SOURCE","taxonomicStatus":"ACCEPTED","nomenclaturalStatus":[],"remarks":"","numDescendants":800354,"lastCrawled":"2017-02-14T13:42:21.926+0000","lastInterpreted":"2017-02-14T13:09:40.175+0000","issues":[],"synonym":false,"_count":905055,"$$hashKey":"object:131"},{"key":5,"nubKey":5,"nameKey":13559875,"taxonID":"gbif:5","kingdom":"Fungi","kingdomKey":5,"datasetKey":"d7dddbf4-2cf0-4f39-9b2a-bb099caae36c","constituentKey":"d7dddbf4-2cf0-4f39-9b2a-bb099caae36c","scientificName":"Fungi","canonicalName":"Fungi","authorship":"","nameType":"SCIENTIFIC","rank":"KINGDOM","origin":"SOURCE","taxonomicStatus":"ACCEPTED","nomenclaturalStatus":[],"remarks":"","publishedIn":"Bot. Mar. 23: 371 (1980)","numDescendants":259402,"lastCrawled":"2017-02-14T13:42:21.926+0000","lastInterpreted":"2017-02-14T13:02:32.067+0000","issues":[],"synonym":false,"_count":84591,"$$hashKey":"object:132"},{"key":7,"nubKey":7,"nameKey":13590911,"taxonID":"gbif:7","kingdom":"Protozoa","kingdomKey":7,"datasetKey":"d7dddbf4-2cf0-4f39-9b2a-bb099caae36c","constituentKey":"d7dddbf4-2cf0-4f39-9b2a-bb099caae36c","scientificName":"Protozoa","canonicalName":"Protozoa","authorship":"","nameType":"SCIENTIFIC","rank":"KINGDOM","origin":"SOURCE","taxonomicStatus":"ACCEPTED","nomenclaturalStatus":[],"remarks":"","numDescendants":18599,"lastCrawled":"2017-02-14T13:42:21.926+0000","lastInterpreted":"2017-02-14T13:36:57.343+0000","issues":[],"synonym":false,"_count":1219,"$$hashKey":"object:133"},{"key":4,"nubKey":4,"nameKey":13556905,"taxonID":"gbif:4","kingdom":"Chromista","kingdomKey":4,"datasetKey":"d7dddbf4-2cf0-4f39-9b2a-bb099caae36c","constituentKey":"d7dddbf4-2cf0-4f39-9b2a-bb099caae36c","scientificName":"Chromista","canonicalName":"Chromista","authorship":"","nameType":"SCIENTIFIC","rank":"KINGDOM","origin":"SOURCE","taxonomicStatus":"ACCEPTED","nomenclaturalStatus":[],"remarks":"","numDescendants":100630,"lastCrawled":"2017-02-14T13:42:21.926+0000","lastInterpreted":"2017-02-14T13:00:47.352+0000","issues":[],"synonym":false,"_count":732,"$$hashKey":"object:134"},{"key":3,"nubKey":3,"nameKey":13555164,"taxonID":"gbif:3","kingdom":"Bacteria","kingdomKey":3,"datasetKey":"d7dddbf4-2cf0-4f39-9b2a-bb099caae36c","constituentKey":"d7dddbf4-2cf0-4f39-9b2a-bb099caae36c","scientificName":"Bacteria","canonicalName":"Bacteria","authorship":"","nameType":"SCIENTIFIC","rank":"KINGDOM","origin":"SOURCE","taxonomicStatus":"ACCEPTED","nomenclaturalStatus":[],"remarks":"","numDescendants":26326,"lastCrawled":"2017-02-14T13:42:21.926+0000","lastInterpreted":"2017-02-14T13:00:10.285+0000","issues":[],"synonym":false,"_count":137,"$$hashKey":"object:135"},{"key":0,"nubKey":0,"nameKey":13592277,"taxonID":"gbif:0","kingdom":"incertae sedis","kingdomKey":0,"datasetKey":"d7dddbf4-2cf0-4f39-9b2a-bb099caae36c","constituentKey":"d7dddbf4-2cf0-4f39-9b2a-bb099caae36c","scientificName":"incertae sedis","canonicalName":"incertae sedis","authorship":"","nameType":"PLACEHOLDER","rank":"KINGDOM","origin":"SOURCE","taxonomicStatus":"DOUBTFUL","nomenclaturalStatus":[],"remarks":"","numDescendants":12499,"lastCrawled":"2017-02-14T13:42:21.926+0000","lastInterpreted":"2017-02-14T13:37:11.829+0000","issues":[],"synonym":false,"_count":94,"$$hashKey":"object:136"}],"endOfRecords":true};
