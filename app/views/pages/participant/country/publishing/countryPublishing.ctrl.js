6eab5952fca76a15ef4b651be3a5a48fe737d946
Morten Høfft <mhoefft@gbif.org>
2018-01-15 16:47:31 +0100

    closes https://github.com/gbif/portal16/issues/654 - show only occ datasets on country pages

--- app/views/pages/participant/country/publishing/countryPublishing.ctrl.js
+++ app/views/pages/participant/country/publishing/countryPublishing.ctrl.js
@@ -1,18 +1,25 @@
 'use strict';
 
-var angular = require('angular');
+var angular = require('angular'),
+    _ = require('lodash');
 
 angular
     .module('portal')
     .controller('countryPublishingCtrl', countryPublishingCtrl);
 
 /** @ngInject */
-function countryPublishingCtrl($stateParams, DatasetSearch) {
+function countryPublishingCtrl($stateParams, DatasetSearch, OccurrenceDatasetSearch) {
     var vm = this;
     vm.countryCode = $stateParams.key;
 
     DatasetSearch.query({publishing_country: vm.countryCode}, function (data) {
         vm.datasetsFrom = data;
+    }, function () {
+        //TODO handle request error
+    });
+
+    OccurrenceDatasetSearch.query({publishing_country: vm.countryCode, facet: 'dataset_key'}, function (data) {
+        vm.occurrenceDatasetsFrom = data;
     }, function () {
         //TODO handle request error
     });
