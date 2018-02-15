66e0441c8afd62e18612b9341a3d2f365e214ab3
Morten Høfft <mhoefft@gbif.org>
2018-02-15 11:51:26 +0100

    log feedback

--- app/models/verification/spam/spam.js
+++ app/models/verification/spam/spam.js
@@ -9,7 +9,9 @@
 
  let urlHelper = require('./urlHelper'),
     _ = require('lodash'),
+    User = require('../../../controllers/api/user/user.model'),
     log = rootRequire('config/log'),
+    moment = require("moment"),
     spamTerms = rootRequire('config/spamTerms');
 
  let terms = spamTerms.map(function(e){return e.toLowerCase()});
@@ -21,12 +23,33 @@ module.exports = {
 function isSpam(message) {
     let referer = _.get(message, 'req.headers.referer'),
         user = _.get(message, 'req.user'),
+        userName = _.get(message, 'req.user.userName'),
+        spamDate = _.get(user, 'systemSettings["spam.date"]'),
+        recentSpammer = spamDate && moment().subtract(7, 'days').isBefore(spamDate);
+
+    let spam = testSpam(message);
+    if (spam) {
+        addSpamDateToUser(user);
+        return true;
+    } else if(recentSpammer) {
+        log.info({module: 'spam', username: userName, referer: referer, title: message.title, text: message.text, reason: 'has spammed recently'});
+        return true;
+    } else {
+       return false;
+    }
+}
+
+function testSpam(message) {
+    let referer = _.get(message, 'req.headers.referer'),
         userName = _.get(message, 'req.user.userName');
 
+    //test that sensible referer
     if (!_.isString(referer) || (!/^http(s)?:\/\/www\.gbif((-dev)|(-uat))?\.org/.test(referer)) && !referer.startsWith('http://localhost:')) {
         log.info({module: 'spam', username: userName, referer: referer, title: message.title, text: message.text, reason: 'invalid referer'});
         return true;
     }
+
+    //test that the content doesn't include blacklisted terms
     for (let i = 0; i < terms.length; i++) {
         let term = terms[i];
         if (testTerm(message.text, term) || testTerm(message.title, term)) {
@@ -38,9 +61,18 @@ function isSpam(message) {
             return true;
         }
     }
+
     return false;
 }
 
 function testTerm(str, term) {
-    return str.toLowerCase().replace(/\.,'"`:;\*_-/g, ' ').replace(/\s\s+/g, ' ').indexOf(term) >= 0
+    return str.toLowerCase().replace(/[.,'"`:;*_\-]/g, '').replace(/\s\s+/g, ' ').indexOf(term) >= 0
+}
+
+function addSpamDateToUser(user) {
+    if (user) {
+        let isodate = new Date().toISOString();
+        _.set(user, 'systemSettings["spam.date"]', isodate);
+        User.update(user.userName, user);
+    }
 }
