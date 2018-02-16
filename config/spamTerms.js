6ecf671614680d1f094f0852b5a988ea3b56d3b6
Morten Høfft <mhoefft@gbif.org>
2018-02-16 09:36:22 +0100

    remove log statement

--- config/spamTerms.js
+++ config/spamTerms.js
@@ -3,6 +3,4 @@ let config = require('./config');
 let spamTerms = require('fs').readFileSync(config.spamTerms, 'utf-8')
     .split('\n').filter(Boolean);
 
-    console.log(spamTerms);
-
 module.exports = Object.freeze(spamTerms);
