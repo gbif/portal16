514f34001a70d72790f1c808a8b5f12235d4c2c6
Morten Hofft <mortenhoefft@gmail.com>
2018-02-21 10:06:34 +0100

    align normalization for term and text

--- app/models/verification/spam/spam.js
+++ app/models/verification/spam/spam.js
@@ -4,22 +4,25 @@
  * We are starting to see spam on the website. So far it isn't from robots, but from flesh and blood spammers that register manually, confirm email and then start posting nonsense.
  * Ideally we integrate with one of the many services to detect spam and where we can also report spam accounts and content.
  *
- * For now we simply strip links (hopefully making it less atrractive) and mark as spam based on our own list of keywords.
+ * For now we simply strip links (hopefully making it less atrractive) and mark as spam based on our own list of keywords. Not perfect, but so far it has turned out to work well enough.
  */
+let urlHelper = require('./urlHelper');
+let _ = require('lodash');
+let User = require('../../../controllers/api/user/user.model');
+let log = require('../../../../config/log');
+let moment = require('moment');
+let spamTerms = require('../../../../config/spamTerms');
 
- let urlHelper = require('./urlHelper'),
-    _ = require('lodash'),
-    User = require('../../../controllers/api/user/user.model'),
-    log = rootRequire('config/log'),
-    moment = require('moment'),
-    spamTerms = rootRequire('config/spamTerms');
-
- let terms = spamTerms.map(function(e) {
-return e.toLowerCase();
-});
+let generatedTerms = generateTerms(spamTerms);
+let terms = generatedTerms.terms;
+let normalizedTerms = generatedTerms.normalizedTerms;
 
 module.exports = {
-    isSpam: isSpam
+    isSpam: isSpam,
+    isSpammingLinks: isSpammingLinks,
+    isSpamReferer: isSpamReferer,
+    isSpamContent: isSpamContent,
+    generateTerms: generateTerms
 };
 
 function isSpam(message) {
@@ -34,10 +37,17 @@ function isSpam(message) {
         addSpamDateToUser(user);
         return true;
     } else if (recentSpammer) {
-        log.info({module: 'spam', username: userName, referer: referer, title: message.title, text: message.text, reason: 'has spammed recently'});
+        log.info({
+            module: 'spam',
+            username: userName,
+            referer: referer,
+            title: message.title,
+            text: message.text,
+            reason: 'has spammed recently'
+        });
         return true;
     } else {
-       return false;
+        return false;
     }
 }
 
@@ -46,29 +56,79 @@ function testSpam(message) {
         userName = _.get(message, 'req.user.userName');
 
     // test that sensible referer
-    if (!_.isString(referer) || (!/^http(s)?:\/\/www\.gbif((-dev)|(-uat))?\.org/.test(referer)) && !referer.startsWith('http://localhost:')) {
-        log.info({module: 'spam', username: userName, referer: referer, title: message.title, text: message.text, reason: 'invalid referer'});
+    if (isSpamReferer(referer)) {
+        log.info({
+            module: 'spam',
+            username: userName,
+            referer: referer,
+            title: message.title,
+            text: message.text,
+            reason: 'invalid referer'
+        });
+        return true;
+    }
+
+    //test that there isn't duplicated links. Spam tend to have that
+    if (isSpammingLinks(message.text)) {
+        log.info({
+            module: 'spam',
+            username: userName,
+            referer: referer,
+            title: message.title,
+            text: message.text,
+            reason: 'duplicate links'
+        });
         return true;
     }
 
     // test that the content doesn't include blacklisted terms
-    for (let i = 0; i < terms.length; i++) {
-        let term = terms[i];
-        if (testTerm(message.text, term) || testTerm(message.title, term)) {
-            log.info({module: 'spam', username: userName, referer: referer, title: message.title, text: message.text, reason: 'text or title considered spam'});
-            return true;
-        }
-        if (urlHelper.hasDuplicateLinks(message.text, {}, 3)) {
-            log.info({module: 'spam', username: userName, referer: referer, title: message.title, text: message.text, reason: 'duplicate links'});
-            return true;
-        }
+    if (isSpamContent(message.text, message.title, terms, normalizedTerms)) {
+        log.info({
+            module: 'spam',
+            username: userName,
+            referer: referer,
+            title: message.title,
+            text: message.text,
+            reason: 'text or title considered spam'
+        });
+        return true;
     }
 
     return false;
 }
 
+function isSpammingLinks(str) {
+    return urlHelper.hasDuplicateLinks(str, {}, 3);
+}
+
+function isSpamContent(text, title, terms, normalizedTerms) {
+    let normalizedText = normalize(text);
+    let normalizedTitle = normalize(title);
+    for (let i = 0; i < terms.length; i++) {
+        let term = terms[i];
+        if (testTerm(text, term) || testTerm(title, term)) {
+            return true;
+        }
+    }
+    for (let i = 0; i < normalizedTerms.length; i++) {
+        let term = normalizedTerms[i];
+        if (testTerm(normalizedText, term) || testTerm(normalizedTitle, term)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function isSpamReferer(referer) {
+    return !_.isString(referer) || (!/^http(s)?:\/\/www\.gbif((-dev)|(-uat))?\.org/.test(referer)) && !referer.startsWith('http://localhost:');
+}
+
 function testTerm(str, term) {
-    return str.toLowerCase().replace(/[.,'"`:;*_\-]/g, '').replace(/\s\s+/g, ' ').indexOf(term) >= 0;
+    return str.indexOf(term) >= 0;
+}
+
+function normalize(str){
+    return str.toLowerCase().replace(/[.,'"`:;*_^~¨\-\s\u{0100}-\u{E007F}]/gu, '');
 }
 
 function addSpamDateToUser(user) {
@@ -77,4 +137,14 @@ function addSpamDateToUser(user) {
         _.set(user, 'systemSettings["spam.date"]', isodate);
         User.update(user.userName, user);
     }
+}
+
+function generateTerms(spamTerms) {
+    let terms = Object.freeze(spamTerms.map(function (e) {
+        return e.toLowerCase();
+    }));
+    let normalizedTerms = Object.freeze(spamTerms.map(function (e) {
+        return normalize(e);
+    }).filter(function(e){return e.length > 0}));
+    return {terms, normalizedTerms};
 }
---
+++ app/models/verification/spam/spam.spec.js
@@ -0,0 +1,46 @@
+let spam = require('./spam.js');
+
+describe('Spam detection', function() {
+    it('can stop spam referers', function() {
+        //obviously wrong
+        expect(spam.isSpamReferer(undefined)).toEqual(true);
+        expect(spam.isSpamReferer(5)).toEqual(true);
+        expect(spam.isSpamReferer('http://something_not_gbif.com')).toEqual(true);
+        expect(spam.isSpamReferer('https://other.org')).toEqual(true);
+
+        //should we allow - we do not for now
+        expect(spam.isSpamReferer('https://subsite.gbif.org')).toEqual(true);
+
+        //accepted
+        expect(spam.isSpamReferer('https://www.gbif.org')).toEqual(false);
+        expect(spam.isSpamReferer('https://www.gbif.org/some/page?anywhere=true')).toEqual(false);
+        expect(spam.isSpamReferer('http://localhost:2000/some/page?anywhere=true')).toEqual(false);
+    });
+
+    it('will block if links are repeated', function() {
+        expect(spam.isSpammingLinks('Something //spamsite.com it is SOO important //spamsite.com to see this //spamsite.com')).toEqual(true);
+        expect(spam.isSpammingLinks('The way vertnet //vertnet.com does it is much better')).toEqual(false);
+    });
+
+    it('will block if content is suspicious', function() {
+        let spamTerms = ['rhino viagra', '[EAT IT]', 'fun-time', '▥'];
+        let generatedTerms = spam.generateTerms(spamTerms);
+        let terms = generatedTerms.terms;
+        let normalizedTerms = generatedTerms.normalizedTerms;
+
+        //should be rejected
+        expect(spam.isSpamContent('Eat this rhino viagra', 'test', terms, normalizedTerms)).toEqual(true, 'spaces');
+        expect(spam.isSpamContent('eat.rhino.viagra', 'test', terms, normalizedTerms)).toEqual(true, 'punctuation');
+        expect(spam.isSpamContent('eat.rhino,viagra', 'test', terms, normalizedTerms)).toEqual(true, 'commas');
+        expect(spam.isSpamContent('eat*rhino*viagra', 'test', terms, normalizedTerms)).toEqual(true, 'astrix');
+        expect(spam.isSpamContent('eat*rhino\n*viagra', 'test', terms, normalizedTerms)).toEqual(true, 'newlines');
+        expect(spam.isSpamContent('eatrhino   *    viagra', 'test', terms, normalizedTerms)).toEqual(true, 'multiple spaces is discarded');
+        expect(spam.isSpamContent('wanna have a fun time?', 'test', terms, normalizedTerms)).toEqual(true, 'missing dashes');
+        expect(spam.isSpamContent('wanna have a fun~¨^time?', 'test', terms, normalizedTerms)).toEqual(true, 'handle a select few symbols');
+        expect(spam.isSpamContent('you should [eat-it]', 'test', terms, normalizedTerms)).toEqual(true, 'hard brackets');
+        expect(spam.isSpamContent('you can filter out unicode ▥ characters', 'test', terms, normalizedTerms)).toEqual(true, 'can block select unicode terms');
+
+        //should be okay
+        expect(spam.isSpamContent('you should be able to write japanese あ', 'test', terms, normalizedTerms)).toEqual(false, 'Japanese is okay');
+    });
+});
