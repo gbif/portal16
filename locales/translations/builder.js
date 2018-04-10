f4584aaed7818cff8868061f651148073243d92e
Morten Hofft <mortenhoefft@gmail.com>
2018-04-10 22:23:16 +0200

    preparing translations #721

--- locales/translations/builder.js
+++ locales/translations/builder.js
@@ -1,7 +1,40 @@
+'use strict';
+
+let _ = require('lodash');
+
 module.exports = builder;
 
 function builder(locale) {
     locale = locale || 'en';
-    let translations = require(`../server/${locale}`);
+    let translations = _.merge(
+        {},
+        require(`./${locale}/enums/backboneIssue`),
+        require(`./${locale}/enums/basisOfRecord`),
+        require(`./${locale}/enums/country`),
+        require(`./${locale}/enums/endpointType`),
+        require(`./${locale}/enums/establishmentMeans`),
+        require(`./${locale}/enums/filterNames`),
+        require(`./${locale}/enums/installationType`),
+        require(`./${locale}/enums/integerEnums`),
+        require(`./${locale}/enums/issueHelp`),
+        require(`./${locale}/enums/iucnStatus`),
+        require(`./${locale}/enums/language`),
+        require(`./${locale}/enums/license`),
+        require(`./${locale}/enums/mediaType`),
+        require(`./${locale}/enums/nameTypeEnum`),
+        require(`./${locale}/enums/nameUsageOrigin`),
+        require(`./${locale}/enums/occurrenceIssue`),
+        require(`./${locale}/enums/originEnum`),
+        require(`./${locale}/enums/protocol`),
+        require(`./${locale}/enums/role`),
+        require(`./${locale}/enums/statusEnum`),
+        require(`./${locale}/enums/taxonRank`),
+        require(`./${locale}/enums/typeStatus`),
+        require(`./${locale}/components/menu`),
+        require(`./${locale}/components/ocurrenceFieldNames`),
+        require(`./${locale}/components/validation`),
+        require(`../server/${locale}`)
+    );
+
     return translations;
 }
