3d8a7c8d46590a385410a01bef09e02cceea6e27
Morten Høfft <mhoefft@gbif.org>
2018-04-11 11:30:52 +0200

    translations #721 splitting file

--- locales/translations/builder.js
+++ locales/translations/builder.js
@@ -8,14 +8,15 @@ function builder(locale) {
     locale = locale || 'en';
     let translations = _.merge(
         {},
-        require(`./${locale}/enums/backboneIssue`),
         require(`./${locale}/enums/basisOfRecord`),
+        require(`./${locale}/enums/continent`),
         require(`./${locale}/enums/country`),
         require(`./${locale}/enums/endpointType`),
         require(`./${locale}/enums/establishmentMeans`),
         require(`./${locale}/enums/filterNames`),
         require(`./${locale}/enums/installationType`),
         require(`./${locale}/enums/integerEnums`),
+        require(`./${locale}/enums/issueEnum`),
         require(`./${locale}/enums/issueHelp`),
         require(`./${locale}/enums/iucnStatus`),
         require(`./${locale}/enums/language`),
@@ -26,13 +27,18 @@ function builder(locale) {
         require(`./${locale}/enums/occurrenceIssue`),
         require(`./${locale}/enums/originEnum`),
         require(`./${locale}/enums/protocol`),
+        require(`./${locale}/enums/region`),
         require(`./${locale}/enums/role`),
         require(`./${locale}/enums/statusEnum`),
+        require(`./${locale}/enums/taxonomicStatus`),
         require(`./${locale}/enums/taxonRank`),
         require(`./${locale}/enums/typeStatus`),
         require(`./${locale}/components/menu`),
         require(`./${locale}/components/ocurrenceFieldNames`),
         require(`./${locale}/components/validation`),
+        require(`./${locale}/components/health`),
+        require(`./${locale}/components/map`),
+        require(`./${locale}/components/profile`),
         require(`../server/${locale}`)
     );
 
