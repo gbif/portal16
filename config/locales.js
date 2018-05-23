96a03a3bde6b1bf08aac2a4816dab9a33bbf8571
Morten Høfft <mhoefft@gbif.org>
2018-05-23 11:31:19 +0200

    closes https://github.com/gbif/portal16/issues/727

--- config/locales.js
+++ config/locales.js
@@ -1,4 +1,14 @@
+// Different systems use different abbrevations and might support different granularities. So unfortunately some mapping is needed.
+// things that can differ: url, contentful, moment, js toLocaleString
+// locales: refer to how it will show in the url
+// moment: moment js naming https://momentjs.com/
+// contentful: the supported name in contentful
+// jsLocale: what is it called when calling toLocaleString
+
+let _ = require('lodash');
 let env = process.env.NODE_ENV || 'local';
+let defaultLocale = 'en';
+
 // locales = ['en' , 'es', 'da', 'en-IN', 'zh-Hant-TW'],
 let locales = ['en'];
 if (env === 'staging' || env === 'local') {
@@ -9,29 +19,35 @@ if (env === 'staging' || env === 'local') {
     locales.push('mock-EU');
     locales.push('mock-ASIA');
 }
-let contentfulLocaleMap = {
-    'en': 'en-GB',
-    'es': 'es',
-    'ar': 'ar',
-    'zh-hant-TW': 'zh',
-    'fr': 'fr',
-    'ru': 'ru',
-    'pt': 'pt'
-};
-let momentMap = {
-    'en': 'en'
-};
+
+let contentfulLocaleMap = _.keyBy(locales);
+// overwrites
+contentfulLocaleMap['en'] = 'en-GB';
+contentfulLocaleMap['es'] = 'es';
+contentfulLocaleMap['ar'] = 'ar';
+contentfulLocaleMap['fr'] = 'fr';
+contentfulLocaleMap['ru'] = 'ru';
+contentfulLocaleMap['pt'] = 'pt';
+contentfulLocaleMap['zh-hant-TW'] = 'zh';
+
+let momentMap = _.keyBy(locales);
+// overwrites
+momentMap['de-CH'] = 'de-ch';
+momentMap['zh'] = 'zh-cn';
 if (env === 'staging' || env === 'local') {
     momentMap['mock-EU'] = '';
     momentMap['mock-ASIA'] = '';
+    momentMap['zh'] = 'zh-cn';
 }
-let defaultLocale = 'en';
+
+let jsLocaleMap = _.keyBy(locales);
 
 module.exports = {
     locales: locales,
     defaultLocale: defaultLocale,
     localeMappings: {
         contentful: contentfulLocaleMap,
-        moment: momentMap
+        moment: momentMap,
+        jsLocale: jsLocaleMap
     }
 };
