af415f5b56f1563d00bb660366e7c37be9e33d8f
Morten Høfft <mhoefft@gbif.org>
2018-05-24 08:48:29 +0200

    extend count directive to support translation strings for 'n something'

--- app/views/components/count/count.directive.js
+++ app/views/components/count/count.directive.js
@@ -3,11 +3,12 @@ var angular = require('angular');
 
 angular
     .module('portal')
-    .directive('count', function($http, $filter, $q, LOCALE) {
+    .directive('count', function($http, $filter, $q, LOCALE, $translate) {
         return {
             restrict: 'A',
             link: function(scope, element, attrs) {
                 var url = attrs.count;
+                var countTranslation = attrs.countTranslation;
                 element.html('<span class="loading"></span>');
 
                 var countPromise = $http.get(url, {
@@ -27,10 +28,16 @@ angular
 
                 promise.then(function(response) {
                     var number = $filter('localNumber')(response.data.count, LOCALE);
-                    element.html(number);
+                    if (countTranslation) {
+                        $translate(countTranslation, {NUMBER: number, NUMBER_FORMATED: number.toLocaleString(LOCALE)}).then(function(translation) {
+                            element.html(translation);
+                        });
+                    } else {
+                        element.html(number);
+                    }
                     element.addClass('loaded');
                 }).catch(function() {
-                    // swallow errors//TODO how to handle this in a gerneal way. in some cases, the count is essential
+                    // swallow errors//TODO how to handle this in a gerneal way. in some cases, the count might be essential. Perhaps then this isn't the directive to use?
                 });
             }
         };
