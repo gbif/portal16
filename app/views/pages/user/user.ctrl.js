daa6b350169b6017ccb7903ea66a58c14c330643
Thomas Stjernegaard <tsjeppesen@gbif.org>
2018-05-24 09:55:35 +0200

    move modal to the main menu component

--- app/views/pages/user/user.ctrl.js
+++ app/views/pages/user/user.ctrl.js
@@ -7,7 +7,7 @@ angular
     .controller('userCtrl', userCtrl);
 
 /** @ngInject */
-function userCtrl(User, Page, $sessionStorage, $scope, AUTH_EVENTS, $state, $uibModal) {
+function userCtrl(User, Page, $sessionStorage, $scope, AUTH_EVENTS, $state) {
     var vm = this;
     vm.$state = $state;
     Page.setTitle('Profile');
@@ -34,50 +34,11 @@ function userCtrl(User, Page, $sessionStorage, $scope, AUTH_EVENTS, $state, $uib
 
     $scope.$on(AUTH_EVENTS.LOGIN_SUCCESS, function() {
         vm.profile = $sessionStorage.user;
-        if (typeof vm.profile.settings.has_read_gdpr_terms === 'undefined') {
-           // alert('Our terms has changed due to the General Data Protection Regulation. Please read the new terms.');
-           openGDPRmodal();
-        }
     });
 
     $scope.$on(AUTH_EVENTS.USER_UPDATED, function() {
         vm.profile = $sessionStorage.user;
     });
-
-   function openGDPRmodal() {
-        var modalInstance = $uibModal.open({
-            animation: true,
-            ariaLabelledBy: 'modal-title',
-            ariaDescribedBy: 'modal-body',
-            templateUrl: 'gdprModal.html',
-            controller: 'gdprModalCtrl',
-            controllerAs: '$ctrl'
-        });
-
-        modalInstance.result.then(function(res) {
-            vm.profile.settings.has_read_gdpr_terms = true;
-            User.update(vm.profile);
-        }, function() {
-            delete vm.profile.settings.has_read_gdpr_terms;
-            User.update(vm.profile);
-            // user clicked cancel
-        });
 }
-}
-
-angular.module('portal').controller('gdprModalCtrl', function($uibModalInstance) {
-    var $ctrl = this;
-    // $ctrl.username;
-    // $ctrl.password;
-    // $ctrl.email;
-
-    $ctrl.ok = function() {
-        $uibModalInstance.close();
-    };
-
-    $ctrl.cancel = function() {
-        $uibModalInstance.dismiss('cancel');
-    };
-});
 
 module.exports = userCtrl;
