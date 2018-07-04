3f25c4e4a8c70b9a2d1345e20b248b5a016f78c2
Morten Hofft <mortenhoefft@gmail.com>
2018-07-04 11:35:48 +0200

    start work on listing onvasive species on species pages

--- app/controllers/api/species/key/speciesKey.ctrl.js
+++ app/controllers/api/species/key/speciesKey.ctrl.js
@@ -173,4 +173,81 @@ router.get('/species/:key/checklistdatasets', function(req, res) {
         });
 });
 
+router.get('/species/:key/invasiveSpecies', function(req, res) {
+    let limit = 500; // get all countries in the world and hope that this publisher only publish one per country and not not invasives
+    let offset = 0;
+    let griisPublisher = 'cdef28b1-db4e-4c58-aa71-3c5238c2d0b5';
+    let result;
+
+    let baseRequest = {
+        url: apiConfig.dataset.url + 'search?taxonKey=' + req.params.key + '&type=CHECKLIST' + '&limit=' + limit + '&offset=' + offset + '&publishingOrg=' + griisPublisher,
+        timeout: 30000,
+        method: 'GET',
+        json: true,
+        fullResponse: true
+    };
+
+    return request(baseRequest)
+        .then(function(response) {
+            result = response;
+            if (parseInt(response.statusCode !== 200)) {
+                throw response;
+            }
+
+            // iterate through datasets to look up species related species
+            let promises = [];
+            response.body.results.forEach(function(e) {
+                promises.push(getInvasiveSpeciesInfo(req.params.key, e.key));
+            });
+
+            return Q.all(promises);
+        })
+        .then(function() {
+            return res.status(200).json(result.body);
+        })
+        .catch(function(err) {
+            log.error(err);
+            let status = err.statusCode || 500;
+            res.sendStatus(status);
+        });
+});
+
+async function getInvasiveSpeciesInfo(taxonKey, dataset) {
+    // get species from this dataset that is related to this taxonkey
+    let related = await request({
+        url: apiConfig.taxon.url + taxonKey + '/related?datasetKey=' + dataset.key,
+        timeout: 30000,
+        method: 'GET',
+        json: true
+        }
+    );
+    if (related.statusCode !== 200) {
+        throw related;
+    }
+
+    let species = related.body.results;
+    // if there is any related species, then there is invasive species listed in that dataset (assuming the provided dataset is listing invasive species)
+    if (species.length > 0) {
+        // extract country from keyword
+        dataset.keywords = dataset.keywords || [];
+        let invasiveInCountry = dataset.keywords.find(function(keyword) {
+            return keyword.startsWith('country_');
+        });
+        if (invasiveInCountry) {
+            invasiveInCountry = invasiveInCountry.replace('country_', '');
+        }
+
+        // compose result obj with the properties we need for displaying the list - no need to send full species and dataaset obj.
+        return {
+            invasiveInCountry: invasiveInCountry,
+            datasetKey: dataset.key,
+            dataset: dataset.title,
+            scientificName: species[0].scientificName,
+            nubKey: species[0].nubKey
+        };
+    } else {
+        return undefined; // TODO ask thomas why he returns this above - i don't see how this would ever be the case
+    }
+}
+
 
