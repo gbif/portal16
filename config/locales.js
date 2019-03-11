f3d180ffb0ad1659c222b20df6f1dcf49048ea65
Thomas Stjernegaard <tsjeppesen@gbif.org>
2019-03-11 10:56:51 +0100

    relates to https://github.com/gbif/portal-feedback/issues/1821

--- config/locales.js
+++ config/locales.js
@@ -10,7 +10,7 @@ let env = process.env.NODE_ENV || 'local';
 let defaultLocale = 'en';
 
 // locales = ['en' , 'es', 'da', 'en-IN', 'zh-Hant-TW'],
-let locales = ['en', 'fr', 'ja', 'pt', 'es'];
+let locales = ['en', 'fr', 'ja', 'pt', 'es', 'zh'];
 if (env === 'local' || env === 'dev') {
     locales.push('da');
     locales.push('de-MOCK');
@@ -20,7 +20,6 @@ if (env === 'local' || env === 'dev') {
 
 if (env === 'uat' || env === 'local') {
     locales.push('zh-tw');
-    locales.push('zh-cn');
     locales.push('ru');
 }
 
