43f6fdaff9723252ad3d2d9d223148463d92ce6d
Morten Høfft <mhoefft@gbif.org>
2019-03-22 12:23:01 +0100

    contentful driven menu

--- app/middleware/menu/menu.js
+++ app/middleware/menu/menu.js
@@ -1,13 +1,40 @@
 /**
- add menu to all requests
+ add menu to all requests.
  */
-let menu = rootRequire('/app/models/menu');
+let menuBuilder = require('./menuBuilder');
+let fallbackMenu = menuBuilder.fallbackMenu;
+let getMenu = menuBuilder.getMenu;
+
+let menus = {};
+async function getMenuData(locale, __) {
+    // if cached version ready, then use that
+    let cachedMenu = menus[locale];
+    let menuPromise = getMenu(locale, __)
+        .then(function(dynamicMenu) {
+            menus[locale] = dynamicMenu;
+            return dynamicMenu;
+        }).catch(function() {
+            return fallbackMenu;
+        });
+    if (cachedMenu) {
+        return cachedMenu;
+    } else {
+        return menuPromise;
+    }
+}
 
 function use(app) {
     app.use(function(req, res, next) {
-        res.locals.gb = res.locals.gb || {};
-        res.locals.gb.menu = menu;
-        next();
+        getMenuData(res.locals.gb.locales.current, req.__)
+            .then(function(menuDynamic) {
+                res.locals.gb = res.locals.gb || {};
+                res.locals.gb.menu = menuDynamic;
+                next();
+            }).catch(function() {
+                res.locals.gb = res.locals.gb || {};
+                res.locals.gb.menu = fallbackMenu;
+                next();
+            });
     });
 }
 
