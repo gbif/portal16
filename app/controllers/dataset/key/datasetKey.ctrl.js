6ad606e4ce46b01713d59880eb227fc5c788eec4
Morten Hofft <mortenhoefft@gmail.com>
2019-05-01 11:51:35 +0200

    relates to https://github.com/gbif/portal-feedback/issues/1669

--- app/controllers/dataset/key/datasetKey.ctrl.js
+++ app/controllers/dataset/key/datasetKey.ctrl.js
@@ -2,6 +2,7 @@
 
 let express = require('express'),
     utils = rootRequire('app/helpers/utils'),
+    _ = require('lodash'),
     helper = rootRequire('app/models/util/util'),
     apiConfig = rootRequire('app/models/gbifdata/apiConfig'),
     request = rootRequire('app/helpers/request'),
@@ -27,14 +28,15 @@ function render(req, res, next) {
         next();
         return;
     }
-    getDataset(apiConfig.dataset.url, datasetKey)
+    getDataset(datasetKey)
         .then(function(dataset) {
             let contentItem = {
                 // key: req.params.key,
                 dataset: dataset,
                 _meta: {
                     title: dataset.title,
-                    description: dataset.description
+                    description: dataset.description,
+                    schema: getMetaSchema(dataset)
                 }
             };
             helper.renderPage(req, res, next, contentItem, 'pages/dataset/key/seo');
@@ -48,7 +50,13 @@ function render(req, res, next) {
         });
 }
 
-async function getDataset(url, key) {
+async function getDataset(key) {
+    let dataset = await getData(apiConfig.dataset.url, key);
+    dataset.publisher = await getData(apiConfig.publisher.url, dataset.publishingOrganizationKey);
+    return dataset;
+}
+
+async function getData(url, key) {
     let options = {
         url: url + key,
         method: 'GET',
@@ -62,4 +70,71 @@ async function getDataset(url, key) {
     return response.body;
 }
 
+function getMetaSchema(dataset) {
+    let authors = _.filter(dataset.contacts, {type: 'ORIGINATOR'}).map(function(contact) {
+        let c = {
+            '@type': 'Person',
+            'givenName': contact.firstName,
+            'familyName': contact.lastName,
+            'email': contact.email[0],
+            'telephone': contact.phone[0],
+            'jobTitle': contact.position,
+            'address': {
+                '@type': 'PostalAddress',
+                'streetAddress': contact.address,
+                'addressLocality': contact.city,
+                'postalCode': contact.postalCode,
+                'addressRegion': contact.province,
+                'addressCountry': contact.country
+            }
+        };
 
+        if (contact.organization) {
+            c.affiliation = {
+                '@type': 'Organization',
+                'name': contact.organization
+            };
+        }
+        return c;
+    });
+
+    let schema = {
+        '@context': 'http://schema.org',
+        '@type': 'Dataset',
+        '@id': 'https://doi.org/' + dataset.doi,
+        'identifier': [
+          {
+            '@type': 'PropertyValue',
+            'propertyID': 'doi',
+            'value': 'https://doi.org/' + dataset.doi
+          },
+          {
+            '@type': 'PropertyValue',
+            'propertyID': 'UUID',
+            'value': dataset.key
+          }
+        ],
+        'url': 'https://www.gbif.org/dataset/' + dataset.key,
+        'name': dataset.title,
+        'author': authors,
+        'description': dataset.description,
+        'license': dataset.license,
+        'inLanguage': dataset.dataLanguage,
+        'datePublished': dataset.created,
+        'dateModified': dataset.modified,
+        'publisher': {
+            '@type': 'Organization',
+            'name': dataset.publisher.title,
+            'url': dataset.publisher.homepage[0]
+          },
+        'provider': {
+          '@type': 'Organization',
+            'name': 'GBIF',
+            'url': 'https://www.gbif.org',
+            'logo': 'https://www.gbif.org/img/logo/GBIF50.png',
+            'email': 'info@gbif.org',
+            'telephone': '+45 35 32 14 70'
+        }
+    };
+    return schema;
+}
