454ee137027fc4e74d6d71466a3f7b90b8d64295
Thomas Stjernegaard <tsjeppesen@gbif.org>
2020-07-02 12:58:33 +0200

    Adjustments to wikipedia summary etc

--- app/views/pages/species/key/directives/wikipediaSummary.directive.js
+++ app/views/pages/species/key/directives/wikipediaSummary.directive.js
@@ -31,7 +31,6 @@ function wikipediaSummaryDirective() {
         }).then(function(res) {
             vm.localeNotFound = !res.data[gb.locale];
             vm.summary = vm.localeNotFound ? _.get(res, 'data.en') : res.data[gb.locale];
-            console.log(vm.summary)
         });
     }
 }
