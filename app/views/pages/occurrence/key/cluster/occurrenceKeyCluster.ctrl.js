79d073850c166ae11b1e4c5d246b1237146815c1
Morten Høfft <mhoefft@gbif.org>
2020-07-08 16:41:08 +0200

    feat: cluster info

--- app/views/pages/occurrence/key/cluster/occurrenceKeyCluster.ctrl.js
+++ app/views/pages/occurrence/key/cluster/occurrenceKeyCluster.ctrl.js
@@ -6,7 +6,7 @@ angular
   .controller('occurrenceKeyClusterCtrl', occurrenceKeyClusterCtrl);
 
 /** @ngInject */
-function occurrenceKeyClusterCtrl($state, $stateParams, OccurrenceRelated, OccurrenceFragment) {
+function occurrenceKeyClusterCtrl($state, $filter, $stateParams, OccurrenceRelated, OccurrenceFragment) {
   var vm = this;
   vm.$state = $state;
   vm.key = $stateParams.key;
@@ -15,14 +15,11 @@ function occurrenceKeyClusterCtrl($state, $stateParams, OccurrenceRelated, Occur
   vm.similarRecords.$promise.then(function(data) {
     data.relatedOccurrences.forEach(function(e) {
       e.occurrence.fragment = OccurrenceFragment.get({id: e.occurrence.gbifId});
-      if (e.reasons) {
-        e.reasons = e.reasons.split(',');
-      }
-      if (!e.occurrence.multimedia) return;
+      if (!e.occurrence.media) return;
       // select first image
-      for (var i = 0; i < e.occurrence.multimedia.length; i++) {
-        if (e.occurrence.multimedia[i].type == 'StillImage') {
-          e.occurrence._image = e.occurrence.multimedia[i];
+      for (var i = 0; i < e.occurrence.media.length; i++) {
+        if (e.occurrence.media[i].type == 'StillImage') {
+          e.occurrence._image = e.occurrence.media[i];
           return;
         }
       }
@@ -33,10 +30,21 @@ function occurrenceKeyClusterCtrl($state, $stateParams, OccurrenceRelated, Occur
     return !!vm.similarRecords.relatedOccurrences;
   };
 
+  vm.formatCoordinates = function(lat, lng) {
+    if (angular.isUndefined(lat) || angular.isUndefined(lng)) {
+        return '';
+    } else {
+        var la = $filter('number')(Math.abs(lat), 1) + (lat < 0 ? 'S' : 'N');
+        var lo = $filter('number')(Math.abs(lng), 1) + (lng < 0 ? 'W' : 'E');
+        return la + ', ' + lo;
+    }
+};
+
   var ggbn = ['Amplification', 'MaterialSample', 'Permit', 'Preparation', 'Preservation'];
-  vm.isSequenced = function(fragment) {
+  vm.isSequenced = function(extensions) {
+    if (!extensions) return false;
     for (var i = 0; i < ggbn.length; i++) {
-      var ext = fragment.extensions['http://data.ggbn.org/schemas/ggbn/terms/' + ggbn[i]];
+      var ext = extensions['http://data.ggbn.org/schemas/ggbn/terms/' + ggbn[i]];
       if (ext && ext.length > 0) return true;
     }
     return false;
