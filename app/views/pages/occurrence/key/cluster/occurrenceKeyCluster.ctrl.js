68b525585fcd2c2d9953094ed78f7a437868f2cb
Morten Høfft <mhoefft@gbif.org>
2020-07-08 22:17:01 +0200

    feat: cluster styling

--- app/views/pages/occurrence/key/cluster/occurrenceKeyCluster.ctrl.js
+++ app/views/pages/occurrence/key/cluster/occurrenceKeyCluster.ctrl.js
@@ -1,20 +1,31 @@
 'use strict';
 var angular = require('angular');
+var _ = require('lodash');
 
 angular
   .module('portal')
   .controller('occurrenceKeyClusterCtrl', occurrenceKeyClusterCtrl);
 
 /** @ngInject */
-function occurrenceKeyClusterCtrl($state, $filter, $stateParams, OccurrenceRelated, OccurrenceFragment) {
+function occurrenceKeyClusterCtrl($q, $state, $filter, $stateParams, Occurrence, OccurrenceRelated, OccurrenceFragment) {
   var vm = this;
   vm.$state = $state;
   vm.key = $stateParams.key;
+
+  vm.currentOccurrence = Occurrence.get({id: vm.key});
   vm.similarRecords = OccurrenceRelated.get({id: vm.key});
 
-  vm.similarRecords.$promise.then(function(data) {
-    data.relatedOccurrences.forEach(function(e) {
+  $q.all({
+    current: Occurrence.get({id: vm.key}).$promise,
+    similarRecords: vm.similarRecords.$promise
+  }).then(function(res) {
+    res.similarRecords.relatedOccurrences.forEach(function(e) {
       e.occurrence.fragment = OccurrenceFragment.get({id: e.occurrence.gbifId});
+      e.occurrence._fullRecord = Occurrence.get({id: e.occurrence.gbifId});
+      e.occurrence._fullRecord.$promise.then(function(response) {
+        e._compare = vm.getFields(res.current, response);
+      });
+
       if (!e.occurrence.media) return;
       // select first image
       for (var i = 0; i < e.occurrence.media.length; i++) {
@@ -38,7 +49,29 @@ function occurrenceKeyClusterCtrl($state, $filter, $stateParams, OccurrenceRelat
         var lo = $filter('number')(Math.abs(lng), 1) + (lng < 0 ? 'W' : 'E');
         return la + ', ' + lo;
     }
-};
+  };
+
+  // eslint-disable-next-line max-len
+  var exclude = ['gbifID', 'key', 'datasetKey', 'publishingOrgKey', 'installationKey', 'protocol', 'lastCrawled', 'lastParsed', 'crawlId', 'lastInterpreted', 'kingdomKey', 'phylumKey', 'classKey', 'orderKey', 'familyKey', 'genusKey', 'speciesKey', 'acceptedTaxonKey'];
+  vm.getFields = function(current, other) {
+    var candidateKeys = _.difference(_.union(Object.keys(current), Object.keys(other)), exclude);
+    candidateKeys.sort();
+    var keys = [];
+    candidateKeys.forEach(function(key) {
+      var currentType = typeof current[key];
+      var otherType = typeof other[key];
+      if (otherType === 'string' || otherType === 'number' || currentType === 'string' || currentType === 'number') {
+        keys.push({
+          key: key,
+          current: current[key],
+          other: other[key],
+          isDifferent: current[key] !== other[key]
+        });
+      }
+    });
+    console.log(keys);
+    return keys;
+  };
 
   var ggbn = ['Amplification', 'MaterialSample', 'Permit', 'Preparation', 'Preservation'];
   vm.isSequenced = function(extensions) {
