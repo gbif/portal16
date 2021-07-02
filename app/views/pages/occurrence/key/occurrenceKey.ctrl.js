f3824427aa5f0ee1c25371d4b5ac79e4b8588c1a
Thomas Stjernegaard <tsjeppesen@gbif.org>
2021-07-02 14:23:40 +0200

    Use http or https for the IIIF viewer, based on manifest protocol

--- app/views/pages/occurrence/key/occurrenceKey.ctrl.js
+++ app/views/pages/occurrence/key/occurrenceKey.ctrl.js
@@ -88,14 +88,14 @@ function occurrenceKeyCtrl($state, $sessionStorage, $stateParams, env, hotkeys, 
            return e['http://rs.tdwg.org/ac/terms/accessURI'];
        });
        if (iiifUris.length > 0) {
-        vm.iiifManifestUri = 'https://labs.gbif.org/mirador/?manifest=' + iiifUris[0];
+        vm.iiifManifestUri = iiifUris[0].split('://')[0] + '://labs.gbif.org/mirador/?manifest=' + iiifUris[0];
        }
    }
     if (!vm.iiifManifestUri && _.get(occurrence, 'dynamicProperties')) {
         try {
            var dynProps = JSON.parse(_.get(occurrence, 'dynamicProperties'));
            if (dynProps['iiifManifestUri']) {
-            vm.iiifManifestUri = 'https://labs.gbif.org/mirador/?manifest=' + dynProps['iiifManifestUri'];
+            vm.iiifManifestUri = dynProps['iiifManifestUri'].split('://')[0] + '://labs.gbif.org/mirador/?manifest=' + dynProps['iiifManifestUri'];
            }
         } catch (err) {
             // unparsable JSON
