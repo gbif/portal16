a4cef5178aa9cb5f82bff9794af08264349c3f36
Thomas Stjernegaard <tsjeppesen@gbif.org>
2021-12-15 12:38:14 +0100

    expose more blast params

--- app/controllers/api/tools/blast/blast.ctrl.js
+++ app/controllers/api/tools/blast/blast.ctrl.js
@@ -1,7 +1,8 @@
 'use strict';
 let express = require('express'),
     router = express.Router(),
-    blast = require('./blast');
+    blast = require('./blast'),
+    _ = require('lodash');
 
 
 module.exports = function(app) {
@@ -9,7 +10,7 @@ module.exports = function(app) {
 };
 
 router.post('/blast', function(req, res) {
-    blast.blast(req.body)
+    blast.blast(req.body, _.get(req, 'query.verbose', false))
     .then((response) => res.status(200).json(response))
     .catch((err) => {
         console.log(err);
