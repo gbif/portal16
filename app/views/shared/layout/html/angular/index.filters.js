6c61078be80763e1628a2a1ffb310338e624d761
Morten Høfft <6759267+MortenHofft@users.noreply.github.com>
2023-08-02 11:39:09 +0200

    formatting

--- app/views/shared/layout/html/angular/index.filters.js
+++ app/views/shared/layout/html/angular/index.filters.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
     'use strict';
     var angular = require('angular'),
         Humanize = require('humanize-plus'),
@@ -23,7 +23,7 @@
     var renderer = new md.Renderer();
 
     // Override function
-    renderer.link = function(href, title, text) {
+    renderer.link = function (href, title, text) {
         title = title || '';
         if (href.substr(0, 1) === '/') {
             href = window.gb.urlPrefix + href;
@@ -33,65 +33,65 @@
 
     angular
         .module('portal')
-        .filter('prettifyEnum', function() {
-            return function(text) {
+        .filter('prettifyEnum', function () {
+            return function (text) {
                 if (typeof text === 'undefined') {
                     return '';
                 }
                 return text.charAt(0) + text.slice(1).toLowerCase().replace(/_/g, ' ');
             };
         })
-        .filter('vocabularyLabel', function(LOCALE_MAPPINGS, LOCALE) {
-          return function(concept) {
-              if (typeof concept === 'undefined') {
-                  return 'Unknown';
-              }
-              var vocabLocale = LOCALE_MAPPINGS.vocabulary[LOCALE] || 'en';
-              // map the concept labels to a map keyed on language
-            if (concept.label) {
-                var labelMap = {};
-                concept.label.forEach(function(label) {
-                    labelMap[label.language] = label.value;
-                });
-                return labelMap[vocabLocale] || labelMap.en || concept.name;
-            } else {
-                return concept.name;
-            }
-          };
-      })
-        .filter('startsWith', function() {
-            return function(text, match) {
+        .filter('vocabularyLabel', function (LOCALE_MAPPINGS, LOCALE) {
+            return function (concept) {
+                if (typeof concept === 'undefined') {
+                    return 'Unknown';
+                }
+                var vocabLocale = LOCALE_MAPPINGS.vocabulary[LOCALE] || 'en';
+                // map the concept labels to a map keyed on language
+                if (concept.label) {
+                    var labelMap = {};
+                    concept.label.forEach(function (label) {
+                        labelMap[label.language] = label.value;
+                    });
+                    return labelMap[vocabLocale] || labelMap.en || concept.name;
+                } else {
+                    return concept.name;
+                }
+            };
+        })
+        .filter('startsWith', function () {
+            return function (text, match) {
                 if (typeof text === 'undefined') {
                     return false;
                 }
                 return text.substr(0, match.length) == match;
             };
         })
-        .filter('truncate', function() {
+        .filter('truncate', function () {
             return filters.truncate;
         })
-        .filter('snakeCase', function() {
-            return function(text) {
+        .filter('snakeCase', function () {
+            return function (text) {
                 return _.snakeCase(text);
             };
         })
-        .filter('camelCase', function() {
-            return function(text) {
+        .filter('camelCase', function () {
+            return function (text) {
                 return _.camelCase(text);
             };
         })
-        .filter('underscoreToHyphen', function() {
-            return function(text) {
+        .filter('underscoreToHyphen', function () {
+            return function (text) {
                 return text.replace(/_/g, '-');
             };
         })
-        .filter('spaceToHyphen', function() {
-            return function(text) {
+        .filter('spaceToHyphen', function () {
+            return function (text) {
                 return text.replace(/\s/g, '-');
             };
         })
-        .filter('flag', function(BUILD_VERSION) {
-            return function(countryCode) {
+        .filter('flag', function (BUILD_VERSION) {
+            return function (countryCode) {
                 if (countryCode) {
                     return '/img/flags/' + _.toUpper(countryCode) + '.png?v=' + BUILD_VERSION;
                 } else {
@@ -99,18 +99,18 @@
                 }
             };
         })
-        .filter('isPast', function() {
-            return function(date) {
+        .filter('isPast', function () {
+            return function (date) {
                 return moment(date).isBefore();
             };
         })
-        .filter('isNew', function() {
-            return function(date) {
+        .filter('isNew', function () {
+            return function (date) {
                 return moment().subtract(90, 'd').isBefore(date);
             };
         })
-        .filter('momentFormat', function(LOCALE, LOCALE_MAPPINGS) {
-            return function(date, format) {
+        .filter('momentFormat', function (LOCALE, LOCALE_MAPPINGS) {
+            return function (date, format) {
                 moment.locale(LOCALE_MAPPINGS.moment[LOCALE]);
                 if (!date) return '';
                 if (format === 'LL') {
@@ -119,39 +119,39 @@
                 return moment(date).format(format || 'LLLL');
             };
         })
-        .filter('momentFormatEnglish', function(LOCALE_MAPPINGS) {
-          return function(date, format) {
-              moment.locale(LOCALE_MAPPINGS.moment['en']);
-              if (!date) return '';
-              if (format === 'LL') {
-                  date = date.substr(0, 10);
-              }
-              return moment(date).format(format || 'LLLL');
-          };
-      })
-        .filter('momentFromNow', function(LOCALE, LOCALE_MAPPINGS) {
+        .filter('momentFormatEnglish', function (LOCALE_MAPPINGS) {
+            return function (date, format) {
+                moment.locale(LOCALE_MAPPINGS.moment['en']);
+                if (!date) return '';
+                if (format === 'LL') {
+                    date = date.substr(0, 10);
+                }
+                return moment(date).format(format || 'LLLL');
+            };
+        })
+        .filter('momentFromNow', function (LOCALE, LOCALE_MAPPINGS) {
             moment.locale(LOCALE_MAPPINGS.moment[LOCALE]);
-            return function(date, format) {
+            return function (date, format) {
                 return moment(date).fromNow();
             };
         })
-        .filter('stripTags', function() {
-            return function(html) {
+        .filter('stripTags', function () {
+            return function (html) {
                 var tmp = document.createElement('DIV');
                 tmp.innerHTML = html;
                 return tmp.textContent || tmp.innerText || '';
             };
         })
-        .filter('compactInteger', function() {
-            return function(nr) {
+        .filter('compactInteger', function () {
+            return function (nr) {
                 return Humanize.compactInteger(nr, 0);
             };
         })
-        .filter('encodeURIComponent', function() {
+        .filter('encodeURIComponent', function () {
             return window.encodeURIComponent;
         })
-        .filter('imgCache', function(env) {
-            return function(imgUrl, width, height) {
+        .filter('imgCache', function (env) {
+            return function (imgUrl, width, height) {
                 if (width || height) {
                     return env.imageCache + (width || '') + 'x' + (height || '') + '/' + window.encodeURIComponent(imgUrl);
                 } else {
@@ -159,8 +159,8 @@
                 }
             };
         })
-        .filter('occurrenceImgCache', function(env) {
-            return function(identifier, occurrenceKey, params) {
+        .filter('occurrenceImgCache', function (env) {
+            return function (identifier, occurrenceKey, params) {
                 if (params) {
                     return env.customImageCache + params + '/occurrence/' + occurrenceKey + '/media/' + md5(identifier);
                 } else {
@@ -168,21 +168,21 @@
                 }
             };
         })
-        .filter('localNumber', function(LOCALE_MAPPINGS, LOCALE) {
-            return function(num, lang) {
+        .filter('localNumber', function (LOCALE_MAPPINGS, LOCALE) {
+            return function (num, lang) {
                 lang = lang ? LOCALE_MAPPINGS.numbers[lang] : (LOCALE_MAPPINGS.numbers[LOCALE] || LOCALE);
                 if (_.isNil(num)) return '';
                 return num.toLocaleString(lang);
             };
         })
-        .filter('startFrom', function() {
-            return function(input, start) {
+        .filter('startFrom', function () {
+            return function (input, start) {
                 start = +start; // parse to int
                 return input.slice(start);
             };
         })
-        .filter('unique', function() {
-            return function(a) {
+        .filter('unique', function () {
+            return function (a) {
                 if (angular.isString(a)) return [a];
                 if (!Array.isArray(a)) return [];
                 var n = {}, r = [];
@@ -195,8 +195,8 @@
                 return r;
             };
         })
-        .filter('uniqueLower', function() {
-            return function(a) {
+        .filter('uniqueLower', function () {
+            return function (a) {
                 if (angular.isString(a)) return [a.toString().toLowerCase()];
                 if (!Array.isArray(a)) return [];
                 var n = {}, r = [];
@@ -210,19 +210,19 @@
                 return r;
             };
         })
-        .filter('authorFirstName', function() {
-            return function(text) {
+        .filter('authorFirstName', function () {
+            return function (text) {
                 return (text) ? text.charAt(0).toUpperCase() + '.' : '';
             };
         })
-        .filter('MathAbs', function() {
-            return function(num) {
+        .filter('MathAbs', function () {
+            return function (num) {
                 return Math.abs(num);
             };
         })
-        .filter('visibleIdentifier', function() {
-            return function(arr) {
-                return arr.filter(function(identifier) {
+        .filter('visibleIdentifier', function () {
+            return function (arr) {
+                return arr.filter(function (identifier) {
                     return [
                         'URL',
                         'LSID',
@@ -235,29 +235,29 @@
                         'GRID',
                         'GRBIO_URI',
                         'GRSCICOLL_URI'
-                        ].indexOf(identifier.type) >= 0;
+                    ].indexOf(identifier.type) >= 0;
                 });
             };
         })
-        .filter('visibleRoles', function() {
-          return function(arr) {
-              return arr.filter(function(role) {
-                var roleString = typeof role === 'string' ? role : role.role;
-                  return [
-                      'HEAD_OF_DELEGATION_ADMINISTRATIVE_CONTACT'
-                      ].indexOf(roleString) === -1;
-              });
-          };
-      })
-        .filter('httpParamSerializer', function($httpParamSerializer) {
-            return function(obj) {
+        .filter('visibleRoles', function () {
+            return function (arr) {
+                return arr.filter(function (role) {
+                    var roleString = typeof role === 'string' ? role : role.role;
+                    return [
+                        'HEAD_OF_DELEGATION_ADMINISTRATIVE_CONTACT'
+                    ].indexOf(roleString) === -1;
+                });
+            };
+        })
+        .filter('httpParamSerializer', function ($httpParamSerializer) {
+            return function (obj) {
                 var query = angular.copy(obj);
                 query = _.omitBy(query, angular.isUndefined);
                 return $httpParamSerializer(query);
             };
         })
-        .filter('formatBytes', function(LOCALE, LOCALE_MAPPINGS) {
-            return function(bytes, decimals, language) {
+        .filter('formatBytes', function (LOCALE, LOCALE_MAPPINGS) {
+            return function (bytes, decimals, language) {
                 language = language ? LOCALE_MAPPINGS.numbers[language] : (LOCALE_MAPPINGS.numbers[LOCALE] || LOCALE);
                 if (bytes == 0) return '0 Bytes';
                 if (bytes == 1) return '1 Byte';
@@ -268,8 +268,8 @@
                 return parseFloat((bytes / Math.pow(k, i)).toFixed(dm)).toLocaleString(language) + ' ' + sizes[i];
             };
         })
-        .filter('formatAsPercentage', function() {
-            return function(percentage, max) {
+        .filter('formatAsPercentage', function () {
+            return function (percentage, max) {
                 var formatedPercentage = 0;
                 if (!isFinite(percentage)) {
                     return percentage;
@@ -300,13 +300,13 @@
                 return formatedPercentage;
             };
         })
-        .filter('parseUrlFilter', function() {
+        .filter('parseUrlFilter', function () {
             // eslint-disable-next-line no-useless-escape
             var urlPattern = /(www|http|ftp|https):\/\/[\w-]+(\.[\w-]+)+([\w.,@?^=%&amp;:\/~+#-]*[\w@?^=%&amp;\/~+#-])?/gi;
-            return function(text, target, clazz) {
+            return function (text, target, clazz) {
                 target = target || '_blank';
                 if (typeof text === 'string') {
-                    angular.forEach(text.match(urlPattern), function(url) {
+                    angular.forEach(text.match(urlPattern), function (url) {
                         var urlPos = text.indexOf(url);
                         if (text.substr(urlPos - 6, 4).indexOf('src') == -1) {
                             text = text.replace(url, '<a target="' + target + '" href=' + url + ' class=' + clazz + '>' + url + '</a>');
@@ -317,13 +317,13 @@
                 return text;
             };
         })
-        .filter('md2html', function() {
-            return function(markdown) {
-                return (markdown) ? md(markdown, {renderer: renderer}) : '';
+        .filter('md2html', function () {
+            return function (markdown) {
+                return (markdown) ? md(markdown, { renderer: renderer }) : '';
             };
         })
-        .filter('gbifUrlAsRelative', function() {
-            return function(url) {
+        .filter('gbifUrlAsRelative', function () {
+            return function (url) {
                 var parseUrl = /^(?:([A-Za-z]+):)?(\/{0,3})([0-9.\-A-Za-z]+)(?::(\d+))?(?:\/([^?#]*))?(?:\?([^#]*))?(?:#(.*))?$/;
                 var result = parseUrl.exec(url);
 
@@ -331,7 +331,7 @@
                 var path = result[5];
                 var locale = (!gb.locale || gb.locale === 'en') ? '' : gb.locale + '/';
                 if (!host) {
-                return url;
+                    return url;
                 }
                 switch (host) {
                     case 'www.gbif.org':
@@ -355,13 +355,13 @@
                 }
             };
         })
-        .filter('capitalizeFirstLetter', function() {
-            return function(string) {
+        .filter('capitalizeFirstLetter', function () {
+            return function (string) {
                 return string.charAt(0).toUpperCase() + string.slice(1);
             };
         })
-        .filter('deCapitalizeFirstLetter', function() {
-            return function(string) {
+        .filter('deCapitalizeFirstLetter', function () {
+            return function (string) {
                 return string.charAt(0).toLowerCase() + string.slice(1);
             };
         });
