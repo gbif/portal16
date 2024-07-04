c1c7f1964cb94b0f9c529b0eef7b2ed61ffc0859
Morten Høfft <6759267+MortenHofft@users.noreply.github.com>
2024-07-04 15:25:03 +0200

    add default caching for all and lower for 404

--- config/express.js
+++ config/express.js
@@ -2,7 +2,7 @@ let express = require('express'),
     helmet = require('helmet'),
     glob = require('glob'),
     passport = require('passport'),
-// favicon = require('serve-favicon'),
+    // favicon = require('serve-favicon'),
     cookieParser = require('cookie-parser'),
     compress = require('compression'),
     methodOverride = require('method-override'),
@@ -11,7 +11,7 @@ let express = require('express'),
     bodyparser = require('body-parser'),
     log = require('./log');
 
-module.exports = function(app, config) {
+module.exports = function (app, config) {
     let env = config.env || 'dev';
     app.locals.ENV = env;
     app.locals.ENV_DEVELOPMENT = env == 'dev';
@@ -44,7 +44,7 @@ module.exports = function(app, config) {
                         'gbif.us18.list-manage.com',
                         'zenodo.org',
                         '*.youtube.com'
-                      ],
+                    ],
                     scriptSrc: [
                         `'self'`,
                         `'unsafe-inline'`,
@@ -58,7 +58,7 @@ module.exports = function(app, config) {
                         'api.mapbox.com',
                         'unpkg.com/react@17/umd/react.production.min.js',
                         'unpkg.com/react-dom@17/umd/react-dom.production.min.js'
-                      ],
+                    ],
                     styleSrc: [
                         `'self'`,
                         `'unsafe-inline'`,
@@ -75,18 +75,18 @@ module.exports = function(app, config) {
                     // frameAncestors: [ // This leads to conflicting statements in Helmet. Looks like we should either go with frameAncestors or x-frame-options
                     //   'https://www.onezoom.org'
                     // ]
-                  }
+                }
 
-          },
-          hsts: {
-            maxAge: 600,
-            includeSubDomains: true
-          }
-    };
-    app.use(helmet(helmetConfig));
+            },
+            hsts: {
+                maxAge: 600,
+                includeSubDomains: true
+            }
+        };
+        app.use(helmet(helmetConfig));
     }
 
-    app.use(function(req, res, next) {
+    app.use(function (req, res, next) {
         if (req.get('host') === 'portal.gbif.org') {
             const header = res.get('Content-Security-Policy');
             if (typeof header === 'string') {
@@ -132,6 +132,23 @@ module.exports = function(app, config) {
         etag: false,
         redirect: false
     }));
+    // Middleware to set default Cache-Control header
+    app.use((req, res, next) => {
+        res.set('Cache-Control', 'public, max-age=600, must-revalidate'); // Default cache for 10 minutes
+        next();
+    });
+
+    // Middleware to set shorter cache for responses with status code above 400
+    app.use((req, res, next) => {
+        const originalSend = res.send;
+        res.send = function (body) {
+            if (res.statusCode >= 400) {
+                res.set('Cache-Control', 'public, max-age=60, must-revalidate'); // Short cache for 1 minute
+            }
+            originalSend.call(this, body);
+        };
+        next();
+    });
     app.use(methodOverride());
     app.use(passport.initialize());
 
@@ -139,7 +156,7 @@ module.exports = function(app, config) {
     require(config.root + '/app/middleware/menu/menu.js').use(app);
 
     // app.use(slashes(false, { code: 302 }));//the module is defect. asking it to remove slashes leads to circular redirects
-    app.use(function(req, res, next) {
+    app.use(function (req, res, next) {
         if (req.path.substr(-1) == '/' && req.path.length > 1) {
             let query = req.url.slice(req.path.length);
             let redirectPath = res.locals.gb.locales.urlPrefix + req.path.slice(0, -1) + query;
@@ -160,7 +177,7 @@ module.exports = function(app, config) {
      require all route controllers
      */
     let controllers = glob.sync(config.root + '/app/controllers/**/*.ctrl.js');
-    controllers.forEach(function(controller) {
+    controllers.forEach(function (controller) {
         require(controller)(app);
     });
 
@@ -178,15 +195,15 @@ module.exports = function(app, config) {
     require(config.root + '/app/errors/404.js')(app);
     require(config.root + '/app/errors/500.js')(app);
 
-    process.on('unhandledRejection', function(reason, p) {
+    process.on('unhandledRejection', function (reason, p) {
         log.error('Unhandled Rejection at root: Promise ', p, ' reason: ', reason);
         // There is not much else to do here. Keep track of the logs and make sure this never happens. There should be no unhandled rejections.
     });
-    process.on('uncaughtException', function(err) {
+    process.on('uncaughtException', function (err) {
         // eslint-disable-next-line no-console
         console.error('FATAL: Uncaught exception.');
         console.error(err.stack || err);
-        setTimeout(function() {
+        setTimeout(function () {
             process.exit(1);
         }, 200);
         log.error('FATAL: Uncaught exception.');
