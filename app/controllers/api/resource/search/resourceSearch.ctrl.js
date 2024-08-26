4115eb094cc1a76a5d3d852b727a516c5c957ea9
Morten Høfft <6759267+MortenHofft@users.noreply.github.com>
2024-08-26 20:20:06 +0200

    miminal referrer test

--- app/controllers/api/resource/search/resourceSearch.ctrl.js
+++ app/controllers/api/resource/search/resourceSearch.ctrl.js
@@ -7,18 +7,21 @@ let express = require('express'),
     resourceResultParser = require('./resourceResultParser'),
     resource = require('../../../resource/key/resourceKey'),
     contentfulLocaleMap = rootRequire('config/config').contentfulLocaleMap,
+    topDomain = rootRequire('config/config').topDomain,
     defaultLocale = rootRequire('config/config').defaultLocale;
 
 module.exports = function(app) {
     app.use('/api', router);
 };
 
+function isExternalRefferer(referrer) {
+    return (typeof referrer !== 'string') || (!/^http(s)?:\/\/(www\.)?gbif((-dev)|(-uat))?\.org/.test(referrer)) && !referer.startsWith('http://localhost:');
+}
+
 router.get('/resource/search', function(req, res) {
-    const origin = req.get('origin');
-    log.info('origin', req.get('origin'));
-    log.info('refferer', req.get('Referrer'));
+    let isExternal = isExternalRefferer(req.get('Referrer'));
     const warningHeader = req.get('not-the-endpoint-you-want');
-    if (warningHeader !== 'See https://techdocs.gbif.org/en/' && req.query.thisIsNotASupportedAPI !== 'true') {
+    if (isExternal && warningHeader !== 'See https://techdocs.gbif.org/en/') {
         res.status(403);
         res.send('Not the endpoint you are looking for. See https://techdocs.gbif.org/en/ or get in touch with help desk if you need some help.');
         return;
