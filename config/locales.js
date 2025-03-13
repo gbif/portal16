bb19d1837533d8f976ae2165193e3e9712d6a943
Daniel Noesgaard <dnoesgaard@gbif.org>
2025-03-13 12:53:53 +0100

    Czech to prod

--- config/locales.js
+++ config/locales.js
@@ -10,7 +10,7 @@ let env = process.env.NODE_ENV || 'local';
 let defaultLocale = 'en';
 
 // locales = ['en' , 'es', 'da', 'en-IN', 'zh-Hant-TW'],
-let locales = ['en', 'ar', 'zh', 'zh-tw', 'pl', 'fr', 'ja', 'pt', 'ru', 'uk', 'es'];
+let locales = ['en', 'ar', 'zh', 'zh-tw', 'pl', 'fr', 'ja', 'pt', 'ru', 'uk', 'es', 'cs'];
 if (env === 'local' || env === 'dev') {
     locales.push('de-MOCK');
     locales.push('ko-MOCK');
@@ -19,7 +19,6 @@ if (env === 'local' || env === 'dev') {
 }
 
 if (env === 'uat' || env === 'local') {
-   locales.push('cs');
    locales.push('it');
 }
 
