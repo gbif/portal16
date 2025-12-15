31d45ebd09efc52acc766b9e0b9f488f1471e7fd
Morten Høfft <6759267+MortenHofft@users.noreply.github.com>
2025-12-15 16:39:12 +0100

    richer logging for failed requests

--- app/controllers/api/loadTest/graphqlRequest.js
+++ app/controllers/api/loadTest/graphqlRequest.js
@@ -15,15 +15,15 @@ function graphqlRequest(query, variables) {
     };
     return request(options, function (error, response, body) {
       if (error) {
-        log.error('Load test request to GraphQL failed: ' + error.message);
+        log.error('Load test request to GraphQL failed: ' + error.message, {query, variables});
         return;
       }
       if (response.statusCode !== 200) {
-        log.error('Load test request to GraphQL failed with status code: ' + response.statusCode);
+        log.error('Load test request to GraphQL failed with status code: ' + response.statusCode, {query, variables});
         return;
       }
       if (body.errors) {
-        log.error('Load test request to GraphQL returned errors: ' + JSON.stringify(body.errors));
+        log.error('Load test request to GraphQL returned errors: ' + JSON.stringify(body.errors), {query, variables});
         return;
       }
     });
